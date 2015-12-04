2bec4e64f2518b9380ceffeaec616c02caef4e91
ryk <erik.desjardins@gmail.com>
2015-12-04 13:47:02 -0500

    Added gitignore to repo, created inbound listener class, updated Package information

---
+++ inbound.js
@@ -0,0 +1,37 @@
+var _c = require('./config.js'),
+    _http = require('http');
+    __inbound = new (function() {
+	var server;
+	var callbacks = {
+		'onRequest' : [],
+		'onConn' : []
+	};
+
+	var handleReq = function(req, resp) {
+		for (var i = 0; i < callbacks.onRequest.length; i++) {
+			callbacks.onRequest[i](req, resp);
+		}
+	}
+
+	var handleConn = function() {
+		for (var i = 0; i < callbacks.onConn.length; i++) {
+			callbacks.onConn[i](req, resp);
+		}
+	};
+	
+	this.bind = function(hook, cb) {
+		callbacks[hook].push(cb);
+	};
+
+	this.start = function() {
+		server.listen(_c.server.port, handleConn);
+	}
+	
+	var init = function() {
+		server = _http.createServer(handleReq);
+	}
+
+	init();
+})();
+
+module.exports = __inbound;
