b35deddf8df48fbd3266cc98fd55c45d74363eea
ryk <erik.desjardins@gmail.com>
2016-01-05 13:49:18 -0500

    Implemented new livevars Livevars now resturn HTTP json header Lilium core was updated

--- backend/static/lilium.js
+++ backend/static/lilium.js
@@ -2,20 +2,56 @@
 // Requires vaniryk
 var LiliumCMS = function() {
 	var LiveVars = function() {
+		var domElems;
+
+		var VarRequest = function(domElems) {
+			var endpoints = [];
+			var url = "";
+
+			this.get = function() {
+				formatURL();
+				
+				_a.get(url, undefined, set);
+			};
+
+			var set = function(data) {
+				console.log(JSON.stringify(data));
+			};
+
+			var formatURL = function() {
+				url = window.location.host + "/livevars?";
+
+				for (var i = 0; i < endpoints.length; i++) {
+					url += (i==0?'':'&') + "vars=" + endpoints[i];
+				}
+			};
+
+			var init = function(domElems) {
+				domElems.all(function(velem) {
+					endpoints.push(velem.data('varname'));
+				});
+			};
 		
+			init(domElems);
+		};
+
+		this.storeDOMelements = function() {
+			domElems = _v('.liliumLiveVar');
+			var req = new VarRequest(domElems);
+
+			req.get();
+		};
 	};
 
 	var Hooks = function() {
-
+		
 	};
 
 	var Rykstrapper = function() {
-
+		
 	};
 
-	this.rykstrapper = new RykStrapper();
-	
-	
+	this.rykstrapper = new RykStrapper();	
 };
 
 var liliumcms = new LiliumCMS();
