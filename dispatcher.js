ab38b7e4bcf393065de4cb11b693b979aef5abf7
samrm111 <samuelrondeaumillaire@gmail.com>
2016-01-11 15:09:08 -0500

    Add support for GET endpoints types

--- dispatcher.js
+++ dispatcher.js
@@ -1,5 +1,5 @@
 var Admin = require('./backend/admin.js');
-var HTMLServer = require('./frontend/htmlserver.js'); 
+var HTMLServer = require('./frontend/htmlserver.js');
 var Endpoints = require('./endpoints.js');
 var LiveVars = require('./livevars.js');
 var _c = require('./config.js');
@@ -7,7 +7,7 @@ var _c = require('./config.js');
 var Dispatcher = function() {
 	this.dispatch = function(cli) {
 		cli.touch('dispatcher.dispatch');
-	
+
 		if (cli.routeinfo.admin) {
 			if (cli.userinfo.loggedin && cli.userinfo.dashaccess) {
 				Admin.serveDashboard(cli);
@@ -22,6 +22,8 @@ var Dispatcher = function() {
 			Admin.serveLogin(cli);
 		} else if (cli.routeinfo.livevars) {
 			LiveVars.handleRequest(cli);
+		}else if (Endpoints.isRegistered(cli.routeinfo.path[0], 'GET')) {
+			Endpoints.execute(cli.routeinfo.path[0], 'GET', cli);
 		} else {
 			HTMLServer.serveClient(cli);
 		}
@@ -36,7 +38,7 @@ var Dispatcher = function() {
 			cli.debug();
 		}
 	};
-	
+
 	var init = function() {
 
 	};
