06649a1e61286d94a6daa34825ce63ff9be9f68c
samrm111 <samuelrondeaumillaire@gmail.com>
2016-01-12 16:04:51 -0500

    add basic crud views/controller for posts

--- backend/admin.js
+++ backend/admin.js
@@ -1,8 +1,10 @@
 var filelogic = require('../filelogic.js');
+var formBuilder = require('../formBuilder');
 
 var Admin = function() {
 	this.serveDashboard = function(cli) {
 		cli.touch('admin.serverDashboard');
+		
 		filelogic.runLogic(cli);
 	};
 
