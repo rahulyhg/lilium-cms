70eb84c036b62c7b2474baa0da40a797553ebde7
samrm111 <samuelrondeaumillaire@gmail.com>
2016-01-12 10:11:26 -0500

    cacheClear command now recreate symlink and uploads folders

--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 var log = require('./log.js');
 var child_process = require('child_process');
-
+var fs = require('./fileserver.js');
+var _c = require('./config.js');
 /**
  * The cli functions for lilium
  * @return {[type]} [description]
@@ -12,7 +13,31 @@ var Cli = function() {
       if (err) {
         log('CLI', err);
       } else {
-        log('CLI', 'Cache cleared');
+        var staticHTMLPath = _c.default.server.html + '/static';
+        fs.dirExists(staticHTMLPath, function(exists) {
+          fs.createDirIfNotExists(_c.default.server.html + '/uploads/', function(valid) {
+            if (valid) {
+              log('CLI',
+                'Upload Directory was validated at : ' +
+                _c.default.server.html + "/uploads/"
+              );
+            } else {
+              log('CLI', 'Error validated upload directory');
+            }
+            //Create symlink
+            if (!exists) {
+              fs.createSymlink(
+                _c.default.server.base + 'backend/static/',
+                staticHTMLPath,
+                function(err) {
+                  log('CLI', 'Cache Cleared');
+                }
+              );
+            }
+          }, true);
+
+        });
+
       }
     });
 
