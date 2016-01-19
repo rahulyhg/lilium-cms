b4406be4ced9448de6575759092a3344b797323d
ryk <erik.desjardins@gmail.com>
2016-01-18 22:10:55 -0500

    Rethought entire LML Logic While and ifs are now working Reduced intepretation time by more than 10 times (!!!) Interpretion now uses Regex for parsing Removed blocking logic, replaced for pointers of line in code using line index Removed bluky lines of code, reduces LMLContext weight

--- lmlcontext.js
+++ lmlcontext.js
@@ -21,24 +21,6 @@ var registeredLibraries = {
 };
 
 var LMLContext = function(info) {
-	this.tagProspect = false;
-	this.isInTag = false;
-	this.isExecTag = false;
-	this.isLMLTag = false;
-
-	this.currentInTag = '';
-	this.cachedCommand = '';
-	this.compiled = '';
-	this.newLine = '';
-	this.skipNextChar = false;
-	this.rootDir = '';
-
-	// Contains : {condTag:"if|while|for", values:[val1, val2], operator:"==|<=|>=|!="}
-	this.condStack = [];
-	this.currentBlock = "lml";
-	this.skipUntilClosure = false;
-	this.storeUntilClosure = false;
-
 	this.touched = ["LMLContext.init"];
 
 	this.lib = {
@@ -60,17 +42,75 @@ var LMLContext = function(info) {
 		this.lib[libName] = registeredLibraries[libName](this);
 	};
 
+	this.states = [];
+	this.stash = function() {
+		this.states.push({
+			tagProspect :       this.tagProspect,
+			isInTag :           this.isInTag,
+			isExecTag :         this.isExecTag,
+			isLMLTag :          this.isLMLTag,
+			storeBuffer : 	    this.storeBuffer,
+			currentInTag :      this.currentInTag,
+			cachedCommand :     this.cachedCommand,
+			newLine :           this.newLine,
+			skipNextChar :      this.skipNextChar,
+			rootDir :           this.rootDir,
+			rootPath :          this.rootPath,
+			avoidParent :       this.avoidParent,
+			lmlBlockThread :    this.lmlBlockThread,
+			currentLineIndex :  this.currentLineIndex,
+			condStack :         this.condStack,
+			currentBlock :      this.currentBlock,
+			skipUntilClosure :  this.skipUntilClosure,
+			storeUntilClosure : this.storeUntilClosure
+		});
+
+		this.init();
+	};
+
+	this.merge = function() {
+		if (this.states.length != 0) {
+			var poped = this.states.pop();
+
+			for (var k in poped) {
+				this[k] = poped[k];
+			};
+		}
+	};
+
 	this.touch = function(str) {
 		this.touched.push(str);
 	};
 
-	var init = function(info) {
-		// Store current context information
+	this.lineFeedback = new Object();
+	this.init = function(info) {
+		this.tagProspect = false;
+		this.isInTag = false;
+		this.isExecTag = false;
+		this.isLMLTag = false;
+
+		this.currentInTag = '';
+		this.cachedCommand = '';
+		this.storeBuffer = '';
+		this.skipNextChar = false;
+		this.rootDir = '';
+		this.rootPath = '';
+		this.avoidParent = false;
+		this.lmlBlockThread = false;
+		this.currentLineIndex = 0;
+
+		// Contains : {condTag:"if|while|for", values:[val1, val2], operator:"==|<=|>=|!="}
+		this.condStack = [];
+		this.currentBlock = "lml";
+		this.skipUntilClosure = false;
+		this.storeUntilClosure = false;
+		
+		// Carried between stashes
+		this.compiled = '';
+		this.newLine = '';
 	};
 
-	if (typeof info !== 'undefined') {
-		init(info);
-	}
+	this.init();
 };
 
 module.exports = LMLContext;
