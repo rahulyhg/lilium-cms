325da072c2caa684e2d1a5babfe0604777d12034
samrm111 <samuelrondeaumillaire@gmail.com>
2016-02-09 15:39:47 -0500

    Remove duplicated ckeditor initialisation

--- backend/static/lilium.js
+++ backend/static/lilium.js
@@ -336,13 +336,6 @@ var LiliumCMS = function() {
       $('[ckeditor]').ckeditor();
       CKEDITOR.on('instanceReady', function(ev) {
         this.document.appendStyleSheet('/static/style.css');
-        ev.editor.dataProcessor.writer.setRules('p', {
-          indent: true,
-          breakBeforeOpen: true,
-          breakAfterOpen: false,
-          breakBeforeClose: false,
-          breakAfterClose: false
-        });
       });
       CKEDITOR.config.entities = false;
       CKEDITOR.config.extraPlugins = 'image-explorer';
@@ -376,7 +369,7 @@ var LiliumCMS = function() {
                 vars: JSON.stringify([{
                   varname: "media",
                   "params": {}
-                }])
+                }]) 
               }, function(data) {
                 pictures = data.media;
                 pictures.forEach(function(picture, index) {
@@ -421,92 +414,7 @@ var LiliumCMS = function() {
         $(selectedPicture).addClass('selectedPicture');
 
       });
-      var pictures = [];
-      var picture_id_url = {};
-      var selectedPicture;
 
-      $('[ckeditor]').ckeditor();
-      CKEDITOR.on('instanceReady', function(ev) {
-        this.document.appendStyleSheet('/static/style.css');
-        ev.editor.dataProcessor.writer.setRules('p', {
-          indent: true,
-          breakBeforeOpen: true,
-          breakAfterOpen: false,
-          breakBeforeClose: false,
-          breakAfterClose: false
-        });
-      });
-      CKEDITOR.config.entities = false;
-      CKEDITOR.config.extraPlugins = 'image-explorer';
-      CKEDITOR.config.basicEntities = false;
-      CKEDITOR.config.entities_greek = false;
-      CKEDITOR.config.entities_latin = false;
-      CKEDITOR.config.removeButtons = 'Image';
-
-      //CKEDITOR plugin to choose picture
-      CKEDITOR.dialog.add("imagebrowserDialog", function(editor) {
-        return {
-          title: "Select an image",
-          minWidth: 800,
-          minHeight: 400,
-          maxWidth: 800,
-          maxHeight: 400,
-          contents: [{
-            id: "tab-step1",
-            label: "Browse for images",
-            elements: [{
-              type: "html",
-              html: '<div id="picture-list"></div>'
-            }]
-          }],
-          onShow: function() {
-            dialog = this;
-            // Check if pictures are loaded first
-            if (pictures.length == 0) {
-              //Load pictures
-              $.get("/livevars", {
-                vars: JSON.stringify([{
-                  varname: "uploads",
-                  "params": {}
-                }])
-              }, function(data) {
-                if (typeof data.uploads !== 'undefined') {
-                  pictures = data.uploads;
-                  pictures.forEach(function(picture, index) {
-                    picture_id_url[picture._id] = '/uploads/' + picture.url;
-
-                    $('#picture-list').append('<img id="' + picture._id + '" src="' + picture.sizes.thumbnail.url + '">');
-                  });
-                } else {
-                  $('#picture-list').html('<h2>No pictures</h2>');
-                }
-              });
-            }
-
-          },
-          onOk: function() {
-            if (typeof selectedPicture !== 'undefined') {
-              var id = $(selectedPicture).attr('id');
-              var img = editor.document.createElement('img');
-              img.setAttribute('src', picture_id_url[id]);
-              img.setStyle('width', '300px');
-              editor.insertElement(img);
-            }
-
-          }
-        }
-      })
-
-      $('body').on('click', '#picture-list img', function(event) {
-        var elem = event.target;
-        if (typeof selectedPicture !== 'undefined') {
-          $(selectedPicture).removeClass('selectedPicture');
-        }
-
-        selectedPicture = elem;
-        $(selectedPicture).addClass('selectedPicture');
-
-      });
     }
   };
 
