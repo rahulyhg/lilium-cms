2ac34ed88e0b87549366d2976c5fe714e37167ab
ryk <erik.desjardins@gmail.com>
2017-02-03 20:02:03 -0500

    Allowed empty petal files to be included using % operator

--- lml/compiler.js
+++ lml/compiler.js
@@ -483,7 +483,7 @@ class LMLTagParser {
 
                 if (toCompile) {
                     fileserver.readFile(fullpath, (ctn) => {
-                        if (!ctn) {
+                        if (typeof ctn == "undefined") {
                             ctx.stream.write(new Error("[LMLPetalExceltion] Undefined content for file " + fullpath).toString());
                             return nextPetal();
                         }
