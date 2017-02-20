05a516e94727fc20fcfe2a53943f775868cf31f6
ryk <erik.desjardins@gmail.com>
2017-02-20 10:21:41 -0500

    Fixed CDN in canonical

--- cdn.js
+++ cdn.js
@@ -17,7 +17,7 @@ var ContentDeliveryNetwork = function() {
                 imgs[i].src = imgs[i].src.replace(domain, cdndomain);
             }
 
-            for (var i = 0; i < links.length; i++) if (links[i].href) {
+            for (var i = 0; i < links.length; i++) if (links[i].href && links[i].rel != "canonical") {
                 links[i].href = links[i].href.replace(domain, cdndomain);
             }
 
