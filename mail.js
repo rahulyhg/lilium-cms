c55997fd065f00d4b0778845a1b654aff02f4bc1
ryk <erik.desjardins@gmail.com>
2017-03-01 19:24:32 -0500

    Fixed mail sender. Works!

--- mail.js
+++ mail.js
@@ -1,11 +1,15 @@
 const log = require('./log.js');
+const google = require('googleapis');
+const fileserver = require('./fileserver.js');
+const nodemailer = require('nodemailer');
 
 let senderInfo = {
     default : {
         user : "",
         pass : "",
         from : "",
-        handler : "gmail"
+        handler : "gmail",
+        transporter : {}
     }
 };
 
@@ -20,6 +24,10 @@ class EMail {
         this.files = [];
     }
 
+    setHTML(html) {
+        this.html = html;
+    }
+
     attach(filepath) {
         this.files.push(filepath)
     }
@@ -27,13 +35,21 @@ class EMail {
 
 class LMLMail {
     setSender(siteid, sender) {
-        if (sender) {
+        if (sender && sender.user && sender.pass) {
             senderInfo[siteid] = senderInfo[siteid] || {};
             log('Mail', 'Updating sender info for website with id ' + siteid);
 
             for (var field in sender) {
                 senderInfo[siteid][field] = sender[field];
             }
+
+            senderInfo[siteid].transporter = nodemailer.createTransport({
+                service : "gmail",
+                auth : {
+                    user : sender.user,
+                    pass : sender.pass
+                }
+            });
         }
     }
 
@@ -47,27 +63,69 @@ class LMLMail {
             callback && callback(new Error("Could not send email : missing fields"));
         } else {
             log('Gmail', 'Sending email from ' + senderInfo[email.siteid].user + ' to ' + email.to);
-            const GMailer = require("gmail-sender");
-            GMailer.options({
-                smtp : {
-                    service : "Gmail",
-                    user : senderInfo[email.siteid].user,
-                    pass : senderInfo[email.siteid].pass,
-                }
-            });
+            let _c = require('./config.js').fetchConfig(email.siteid);
 
-            GMailer.send({
-                to : { email : email.to },
+            let emailobj = {
+                from : '"' + senderInfo[email.siteid].from + '" <' + senderInfo[email.siteid].user + ">",
+                to : email.to,
                 subject : email.subject,
-                text : email.content,
-                // files : email.files
-            }, (err, res) => {
+            };
+
+            if (email.html) {
+                emailobj.html = email.html;
+            } else {
+                emailobj.text = email.content;
+            }
+
+            senderInfo[email.siteid].transporter.sendMail(emailobj, (err, res) => {
                 if (err) {
                     log('Mail', 'Could not send email => ' + err, 'err');
+                } else {
+                    log('Mail', 'Sent email successfully to ' + email.to, 'success');
                 }
 
                 callback && callback(err, res);
             });
+
+            /*
+            fileserver.fileExists(_c.emails.gmailkey, function(exists) {
+                if (!exists) {
+                    return callback && callback(new Error("Missing gmail JSON key file"));
+                }
+
+                const googleKey = require(_c.emails.gmailkey);
+                const jwtClient = new google.auth.JWT(
+                    googleKey.client_email, 
+                    null, 
+                    googleKey.private_key, 
+                    ['https://www.googleapis.com/auth/gmail.send'], 
+                    senderInfo[email.siteid].user
+                );
+
+                jwtClient.authorize((err, tokens) => {
+                    if (err) {
+                        log('Mail', 'Could not send email because of JWT error => ' + err, 'err');
+                        return callback && callback (err);
+                    }
+
+                    var gmail = google.gmail({version: 'v1'});
+                    gmail.users.messages.send({
+                        auth: jwtClient,
+                        userId : senderInfo[email.siteid].user,
+                        uploadType : 'media',
+                        ressource : {
+                            raw : require('js-base64').Base64.encodeURI(email.content)
+                        }
+                    }, (err, res) => {
+                        if (err) {
+                            log('Mail', 'Could not send email => ' + err, 'err');
+                        }
+
+                        callback && callback(err, res);
+                    });
+                });
+            });
+            */
         }
     }
 
