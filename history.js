e1fb30267275c7eda7e97e7119ef8ecbb3b1882b
ryk <erik.desjardins@gmail.com>
2017-03-26 17:53:45 -0400

    Added Utils class usable by pretty much everything

--- history.js
+++ history.js
@@ -30,9 +30,9 @@ class History {
                         default : diffobj.change = "unknown";   
                     }
 
-                    if (diffobj.change == "modified" && !utils.pulloutProp(oldState, diff.path)) {
+                    if (diffobj.change == "modified" && !utils.objects.pulloutProp(oldState, diff.path)) {
                         diffobj.change = "added";
-                    } else if (diffobj.change == "modified" && !utils.pulloutProp(newArticle, diff.path)) {
+                    } else if (diffobj.change == "modified" && !utils.objects.pulloutProp(newArticle, diff.path)) {
                         diffobj.change = "removed";
                     }
 
