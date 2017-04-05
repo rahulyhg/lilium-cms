a3c85feb29d0fa37410b399e500c4d56acce9cd2
ryk <erik.desjardins@gmail.com>
2017-04-05 02:24:20 -0400

    Merged Riverflow and Topics Created Reporting Flow

---
+++ reporting.js
@@ -0,0 +1,10 @@
+const log = require('./log.js');
+const db = require('./includes/db.js');
+
+class Reporting {
+    livevar(cli, levels, params, send) {
+
+    }
+}
+
+module.exports = new Reporting();
