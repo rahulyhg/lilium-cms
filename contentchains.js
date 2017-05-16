0cbf2e90f84e186c45769c8ed7ed432eacecc847
Erik Desjardins <erik.desjardins@gmail.com>
2017-05-16 04:39:42 -0400

    Intergrated Draggable library, bower install required Finished LMLDom upgrades Removed table from riverflow chains Included new file in sites precompilation admin side

--- contentchains.js
+++ contentchains.js
@@ -12,19 +12,49 @@ class ContentChains {
         return Object.assign({
             title : "",
             subtitle : "",
+            slug : "",
             presentation : "",
-            status : "draft",
+            status : "backstage",
             createdBy : undefined,
             createdOn : new Date(),
             lastModified : new Date(),
-            publishedOn : new Date(),
-            media : undefined
+            media : undefined,
+            serie : []
         }, data);
     }
 
     insertNewChain(_c, data, callback) {
         let newChain = this.createFromObject(data);
         db.insert(_c, 'contentchains', newChain, callback);
+    }
+
+    handleStatusChange(_c, id, callback) {
+        this.deepFetch(_c, id, cc => {
+            
+        });
+    }
+
+    editChain(_c, id, data, callback) {
+        let chainData = data;
+        if (chainData.media) {
+            chainData.media = db.mongoID(chainData.media);
+        }
+
+        if (chainData.date) {
+            chainData.date = new Date(chainData.date);
+        } else {
+            delete chainData.date;
+        }
+
+        let that = this;
+        id = db.mongoID(id);
+        db.update(_c, 'contentchains', {_id : id}, chainData, () => {
+            if (chainData.status) {
+                that.handleStatusChange.apply(that, [_c, id, callback]);
+            } else {
+                callback && callback();
+            }
+        });
     }
 
     deepFetch(_c, chainid, send) {
@@ -34,7 +64,51 @@ class ContentChains {
     }
 
     bunchLivevar(cli, levels, params, send) {
-        send([]);
+        let filters = params.filters || {};
+        let page = (params.page || 1) - 1;
+        let max = params.max || 25;
+
+        let query = {};
+
+        // Allowed filters
+        if (filters.status) {
+            query.status = filters.status;
+        }
+
+        if (max < 1) {
+            max = 25;
+        }
+
+        db.join(cli._c, 'contentchains', [{
+            $match : query
+        }, {
+            $sort : {_id : -1}
+        }, {
+            $skip : ((page < 0 ? 0 : page) * max)
+        }, {
+            $limit : max
+        }, {
+            $lookup : {
+                from : "uploads",
+                localField : "media",
+                foreignField : "_id",
+                as : "featuredimage"
+            }
+        }], (items) => {
+            items.forEach(item => {
+                let img = item.featuredimage.pop();
+                if (img) {
+                    item.featuredimageurl = img.sizes.square.url;
+                }
+
+                item.featuredimage = undefined;
+                item.media = undefined;
+            });
+
+            db.count(cli._c, 'contentchains', query, (err, total) => {
+                send({ items, total });
+            });
+        });
     }
 
     livevar(cli, levels, params, send) {
@@ -85,7 +159,12 @@ class ContentChains {
                 });
             });
         } else if (path == "edit") {
-            
+            this.editChain(cli._c, cli.routeinfo.path[3], cli.postdata.data, (error) => {
+                cli.sendJSON({
+                    success : !error,
+                    error 
+                });
+            });
         } else {
             cli.throwHTTP(404, undefined, true);
         }
@@ -123,19 +202,41 @@ class ContentChains {
         .add('presentation', 'ckeditor', {
             nolabel : true
         })
+        .add('media', 'media-explorer', {
+            displayname : "Featured image",
+            classes : "content-chain-media-picker"
+        })
         .add('title-details', 'title', {
             displayname : "Details"
         })
-        .add('publish-set', 'buttonset', { buttons : [{
-                'name' : 'save',
-                'displayname': 'Save changes',
-                'type' : 'button',
-                'classes': ['btn-save']
+        .add('date', 'date', {
+            displayname : "Live date",
+            datetime : true,
+            context : 'edit'
+        }, {
+            require : false
+        })
+        .add('title-actions', 'title', {
+            displayname : "Actions"
+        })
+        .add('publish-set', 'buttonset', { buttons : [
+            {
+                name : 'save',
+                displayname: 'Save changes',
+                type : 'button',
+                classes: ['btn-save']
+            }, {
+                name : "golive",
+                displayname : "Save and go Live",
+                type : "button",
+                classes : ["btn-publish", "green"]
+            }, {
+                name : "backstage",
+                displayname : "Backstage",
+                type : "button",
+                classes : ["btn-remove"]
             }
         ]});
-    }
-
-    table(cli) {
     }
 }
 
