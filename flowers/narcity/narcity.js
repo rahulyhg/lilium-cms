6ed404b90c55c568b9a685689614227fa53b5f45
ryk <erik.desjardins@gmail.com>
2017-08-14 13:16:21 -0400

    Fixed language exception in API

--- flowers/narcity/narcity.js
+++ flowers/narcity/narcity.js
@@ -1667,7 +1667,7 @@ var parseAds = function(pkg) {
 
         if (art.content.includes("<lml-related>") && !pkg.article.isSponsored && !pkg.article.paginated) {
             var scr = require('./cxense.js')(
-                require("./vocab/" + (pkg.extra.language || art.topic.override.language || _c.website.language || "en-ca")), 
+                require("./vocab/" + (pkg.extra.language || (art.topic.override && art.topic.override.language) || _c.website.language || "en-ca")), 
                 '8ddab9eb8cc002f512315b910672a13537f0ca00'
             );
 
