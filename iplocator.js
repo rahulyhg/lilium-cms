8e637e1c74ca80184b01d65dc852fa22d83aae3a
Erik <erik.desjardins@gmail.com>
2017-09-09 02:11:03 -0400

    Added request balancer to IPLocator

--- iplocator.js
+++ iplocator.js
@@ -5,7 +5,77 @@ const config = require('./config');
 
 const requestQueue = [];
 
-const API_URL = "https://tools.keycdn.com/geo.json?host=";
+let STRATEGY_ROTATION = 0;
+let STRATEGIES = ["Nekudo", "KeyCDN"];
+
+class IPStrategy {
+    static useOne(ip, userid, done) {
+        STRATEGY_ROTATION++;
+        if (STRATEGY_ROTATION == STRATEGIES.length) {
+            STRATEGY_ROTATION = 0;
+        }
+
+        IPStrategy[STRATEGIES[STRATEGY_ROTATION]](ip, userid, done);
+    }
+
+    static Nekudo(ip, userid, done) {
+        const API_URL = "http://geoip.nekudo.com/api/";
+
+        request({
+            url : API_URL + ip,
+            method : "GET",
+            json : true
+        }, (err, msg, json) => {
+            if (json && json.type != "error") {
+                const geo = json;
+                db.update(config.default(), 'entities', { _id : db.mongoID(userid) }, { 
+                    geo : {
+                        latitude : geo.location.latitude,
+                        longitude : geo.location.longitude,
+                        timezone : geo.location.time_zone,
+                        city : geo.city,
+                        country : geo.country.name,
+                        ip : ip,
+                        updatedAt : new Date()
+                    }
+                }, () => {
+                    done();
+                });
+
+            } else {
+                done();
+            }
+        });
+    }
+
+    static KeyCDN(ip, userid, done) {
+        const API_URL = "https://tools.keycdn.com/geo.json?host=";
+        request({
+            url : API_URL + ip,
+            method : "GET",
+            json : true
+        }, (err, msg, json) => {
+            if (json && json.status == "success" && json.data.geo && json.data.geo.timezone) {
+                const geo = json.data.geo;
+                db.update(config.default(), 'entities', { _id : db.mongoID(userid) }, { 
+                    geo : {
+                        latitude : geo.latitude,
+                        longitude : geo.longitude,
+                        timezone : geo.timezone,
+                        city : geo.city,
+                        country : geo.country_name,
+                        ip : ip,
+                        updatedAt : new Date()
+                    }
+                }, () => {
+                    done();
+                });
+            } else {
+                done();
+            }
+        });
+    }
+}
 
 let running = false;
 class IPLocator {
@@ -20,19 +90,7 @@ class IPLocator {
                     return;
                 }
 
-                request({
-                    url : API_URL + nxt.ip,
-                    method : "GET",
-                    json : true
-                }, (err, msg, json) => {
-                    if (json && json.status == "success") {
-                        const geo = json.data.geo;
-                        geo.updatedAt = new Date();
-                        db.update(config.default(), 'entities', { _id : db.mongoID(nxt.userid) }, { geo }, () => {
-
-                        });
-                    }
-
+                IPStrategy.useOne(nxt.ip, nxt.userid, () => {
                     setTimeout(() => {
                         nextIP();
                     }, 1100);
