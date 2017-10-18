b18e3171ab9348dcd65c7d0d224365cb4949ecc7
Erik Desjardins <erik.desjardins@gmail.com>
2017-10-18 01:13:40 -0400

    Vocab library ES6 porting

--- vocab.js
+++ vocab.js
@@ -1,27 +1,27 @@
-var _c = require('./config.js');
-var dicos = {};
-var fileserver = require('./fileserver.js');
-var log = require('./log.js');
+const _c = require('./config.js');
+const dicos = {};
+const fileserver = require('./fileserver.js');
+const log = require('./log.js');
 
-var Vocab = function() {};
-
-Vocab.prototype.preloadDicos = function(done) {
-    log("Vocab", "Preloading dictionaries", "info");
-    fileserver.listDirContent('./vocab/', function(files) {
-        for (var i = 0; i < files.length; i++) {
-            if (files[i].indexOf('.json') != -1) {
-                dicos[files[i].substring(0, files[i].length - 3)] = require("./vocab/" + files[i]);
-                log("Vocab", "Loading dico : " + files[i], "detail");
+class Vocab {
+    preloadDicos(done) {
+        log("Vocab", "Preloading dictionaries", "info");
+        fileserver.listDirContent('./vocab/', function(files) {
+            for (let i = 0; i < files.length; i++) {
+                if (files[i].indexOf('.json') != -1) {
+                    dicos[files[i].substring(0, files[i].length - 3)] = require("./vocab/" + files[i]);
+                    log("Vocab", "Loading dico : " + files[i], "detail");
+                }
             }
-        }
 
-        log("Vocab", "Preloaded dictionaries", "success");
-        done();
-    });
-};
+            log("Vocab", "Preloaded dictionaries", "success");
+            done();
+        });
+    }
 
-Vocab.prototype.getDico = function(lang) {
-    return dicos[lang];
+    getDico(lang) {
+        return dicos[lang];
+    }
 };
 
 module.exports = new Vocab();
