12b5386e106b7c828b9ca63f3bb2b54ce739a010
Erik <erik.desjardins@gmail.com>
2017-11-01 11:13:13 -0500

    Modified the update script with new (sub)title array

--- updates/2.1.0.js
+++ updates/2.1.0.js
@@ -7,7 +7,10 @@ module.exports = (conf, done) => {
             if (hasnext) {
                 cur.next((err, article) => {
                     article.content = article.content.split("<lml-page></lml-page>");
-                    db.update(conf, 'content', {_id : article._id}, {content : article.content}, () => {
+                    article.title = new Array(article.content.length).fill(article.title);
+                    article.subtitle = new Array(article.content.length).fill(article.subtitle); 
+
+                    db.update(conf, 'content', {_id : article._id}, {content : article.content, title : article.title, subtitle : article.subtitle}, () => {
                     log("Update", "Updated article " + article.title, "lilium");
                         next(cur);
                     });
@@ -21,5 +24,5 @@ module.exports = (conf, done) => {
     log("Update", "UPDATING TO NEW PAGINATION", "lilium");
     db.find(conf, 'content', {content : {$exists : 1}}, [], (err, cur) => {
         next(cur);
-    }, {content : 1, title : 1, _id : 1});
+    }, {content : 1, title : 1, subtitle : 1, _id : 1});
 };
