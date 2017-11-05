01cd8e1c60398034e96a8e6fce917a120ff04570
Erik Desjardins <erik.desjardins@gmail.com>
2017-11-05 18:10:30 -0500

    Update script to V2.1.1

---
+++ updates/2.1.1.js
@@ -0,0 +1,27 @@
+const db = require('../includes/db');
+const log = require('../log');
+
+module.exports = (conf, done) => {
+    let next = (cur) => {
+        cur.hasNext((err, hasnext) => {
+            if (hasnext) {
+                cur.next((err, article) => {
+                    article.facebooktitle = article.title && article.title[0];
+                    article.facebooksubtitle = article.subtitle && article.subtitle[0];
+
+                    db.update(conf, 'content', {_id : article._id}, {facebooktitle : article.facebooktitle, facebooksubtitle : article.facebooksubtitle}, () => {
+                    log("Update", "Updated article " + article.title, "lilium");
+                        next(cur);
+                    });
+                });
+            } else {
+                done();
+            }
+        });
+    };
+
+    log("Update", "UPDATING TO NEW FACEBOOK FIELD", "lilium");
+    db.find(conf, 'content', {status : "published"}, [], (err, cur) => {
+        next(cur);
+    }, {content : 1, title : 1, subtitle : 1, _id : 1});
+};
