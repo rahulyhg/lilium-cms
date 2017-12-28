c1b822b69a20f4e687db5b62c5f63100fc0d42af
Erik Desjardins <erik.desjardins@gmail.com>
2017-12-27 22:17:35 -0500

    Full Twilio support with JSON config file #61

---
+++ sms.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const log = require('./log');
+
+class SMS {
+    getConfig() {
+        try {
+            return require('../keys/twilio.json');
+        } catch (ex) {
+            return { error : ex };
+        }
+    }
+
+    formatURL(conf) {
+        return `https://${conf.sid}:${conf.token}@api.twilio.com/2010-04-01/Accounts/${conf.sid}/Messages.json`;
+    }
+
+    sendTo(to, message, done) {
+        const conf = this.getConfig();
+        if (!conf.error) {
+            const url = this.formatURL(conf);
+            log('SMS', 'Sending SMS to ' + to);
+            request.post(url, { form : {
+                To : to, From : conf.from, Body : message
+            } }, (err, resp, body) => {
+                err && log("SMS", "Error sending SMS. From Twilio : " + body, 'err');
+                done(!err);
+            });
+        } else {
+            log('SMS', 'Error sending SMS : Missing Twilio config file', 'err');
+            done(false);
+        }
+    }
+}
+
+module.exports = new SMS();
