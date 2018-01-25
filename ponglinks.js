4c053405b3f2c3532a02d75515834d6bc09a7027
ryk <erik.desjardins@gmail.com>
2018-01-25 16:22:41 -0500

    Added adminbar menu item and livevar #67

--- ponglinks.js
+++ ponglinks.js
@@ -1,10 +1,20 @@
 const db = require('./includes/db');
 const sharedcache = require('./sharedcache');
 const isElder = require('./network/info').isElderChild();
+const SHA256 = (str) => require('crypto-js').SHA256(str).toString();
+const filelogic = require('./filelogic');
 
 class PongLinks {
-    adminGET(cli) {
+    hashDestination(dest) {
+        return SHA256(dest);
+    }
 
+    adminGET(cli) {
+        if (!cli.hasRight('ponglinks')) {
+            return cli.refuse();
+        }
+
+        filelogic.serveAdminLML3(cli);
     }
 
     adminPOST(cli) {
@@ -12,11 +22,19 @@ class PongLinks {
     }
 
     GET(cli) {
-        
+        const hash = cli.routeinfo.path[1];
+        sharedcache.get("ponglinks_" + hash, domain => domain ? cli.redirect(domain) : cli.throwHTTP(404, undefined, true));
     }
 
     livevar(cli, levels, params, sendback) {
-
+        sendback({ items : [{
+            identifier : "Testin and ballin",
+            status : "active",
+            outbound : "www.liliumcms.com",
+            hash : "a73bd6e7bf737bad837bf82901fba98be78a9c83b7c8ad73",
+            uniqueclicks : 13802,
+            clicks : 12112
+        }]});
     }
 
     setup() {
@@ -24,9 +42,9 @@ class PongLinks {
             require('./config').each((site, next) => {
                 db.createCollection(site, 'ponglinks', () => {
                     db.findToArray(site, 'ponglinks', { active : true }, (err, arr) => {
-                        log('Ponglink', 'Storing ' + arr.length + " links in shared cache");
+                        log('Ponglinks', 'Storing ' + arr.length + " links in shared cache");
                         arr.forEach(x => {
-                            sharedcache.set({ ["ponglink_" + x.hash] : x.destination });
+                            sharedcache.set({ ["ponglinks_" + x.hash] : x.destination });
                         });
 
                         next();
