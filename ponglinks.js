7e35fd112c918e5c2247040073090d8ed431e5fe
Erik Desjardins <erik.desjardins@gmail.com>
2018-01-25 21:06:52 -0500

    LMLBunch livevar filter integration

--- ponglinks.js
+++ ponglinks.js
@@ -4,9 +4,24 @@ const isElder = require('./network/info').isElderChild();
 const SHA256 = (str) => require('crypto-js').SHA256(str).toString();
 const filelogic = require('./filelogic');
 
+const ALLOWED_EDIT_FIELDS = [
+    "destination", "identifier", "status"
+];
+
 class PongLinks {
     hashDestination(dest) {
         return SHA256(dest);
+    }
+
+    parseEditFields(keyval) {
+        const edit = {};
+        ALLOWED_EDIT_FIELDS.forEach(x => {
+            if (keyval[x]) {
+                edit[x] = keyval[x];
+            }
+        });
+
+        return edit;
     }
 
     adminGET(cli) {
@@ -17,8 +32,38 @@ class PongLinks {
         filelogic.serveAdminLML3(cli);
     }
 
-    adminPOST(cli) {
+    editLink(_c, _id, keyval, done) {
+        db.update(_c, 'ponglinks', { _id }, this.parseEditFields(keyval), (err, r) => done(err, !!r.modifiedCount));
+    }   
 
+    createLink(_c, creatorid, link, done) {
+        db.insert(_c, 'ponglinks', {
+            creatorid, 
+            createdOn : new Date(),
+            createdAt : Date.now(),
+
+            status : "active", 
+            identifier : link.identifier,
+            destination : link.destination,
+            hash : this.hashDestination(link.identifier + link.destination),
+            uniqueclicks : 0,
+            clicks : 0
+        }, (err, r) => done(err, r.insertedId));
+    }
+
+    adminPOST(cli) {
+        if (!cli.hasRight('ponglinks')) {
+            return cli.refuse();
+        }
+
+        const action = cli.routeinfo.path[2];
+        if (action == "create") {
+            this.createLink(cli._c, db.mongoID(cli.userinfo.userid), cli.postdata.data, (err, id)  => cli.sendJSON(err ? { error : err.toString() } : { id }) );
+        } else if (action == "edit") {
+            this.editLink(cli._c, db.mongoID(cli.routeinfo.path[3]), cli.postdata.data, (err, mod) => cli.sendJSON(err ? { error : err.toString() } : { mod }));
+        } else {
+            cli.throwHTTP(404, undefined, true);
+        }
     }
 
     GET(cli) {
@@ -27,14 +72,19 @@ class PongLinks {
     }
 
     livevar(cli, levels, params, sendback) {
-        sendback({ items : [{
-            identifier : "Testin and ballin",
-            status : "active",
-            outbound : "www.liliumcms.com",
-            hash : "a73bd6e7bf737bad837bf82901fba98be78a9c83b7c8ad73",
-            uniqueclicks : 13802,
-            clicks : 12112
-        }]});
+        const $match = { status : "active" };
+
+        if (params.filters.search) {
+            $match.identifier = new RegExp(params.filters.search, 'i');
+        }
+
+        if (params.filters.status) {
+            $match.status = params.filters.status;
+        }
+
+        db.findToArray(cli._c, 'ponglinks', $match, (err, items) => {
+            sendback({ items });
+        });
     }
 
     setup() {
