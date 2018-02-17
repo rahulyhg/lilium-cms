a1c3f971e74c8a69ec200b084c668aed63619d33
Erik Desjardins <erik.desjardins@gmail.com>
2018-02-17 01:29:04 -0500

    Writed new Content Controller with contentlib

--- contentcontroller.js
+++ contentcontroller.js
@@ -6,6 +6,7 @@ class ContentController {
         // Get LML3 markup
         switch (cli.routeinfo.path[2]) {
             case "list" : cli.hasRightOrRefuse('list-articles') && filelogic.serveAdminLML3(cli); break;
+            case "write" : cli.hasRightOrRefuse('create-articles') && filelogic.serveAdminLML3(cli, true); break;
             default : cli.refuse();
         }
     }
@@ -29,6 +30,8 @@ class ContentController {
         // Get formatted data
         if (levels[0] == "bunch") {
             contentlib.bunch(cli._c, params.filters, params.filters && params.filters.sort, params.max, (params.page - 1) * params.max, sendback);
+        } else if (levels[0] == "write") {
+            contentlib.getFull(cli._c, levels[1], post => sendback(post));
         } else {
             sendback([]);
         }
