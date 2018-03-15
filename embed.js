27c08615c098b4597c7afd734286b18ed16920b3
Erik Desjardins <erik.desjardins@gmail.com>
2018-03-15 01:25:19 -0400

    V3 insta embed via icon fixed

--- embed.js
+++ embed.js
@@ -15,7 +15,7 @@ var createV3DivFromResponse = function(data, ourl) {
     return '<p><img data-width="'+data.dimensions.width+'" data-height="'+data.dimensions.height+'" src="'+data.display_url+                  
         '" class="lml-instagram-embed-3" /><a class="lml-instagram-op-3" href="'+
         ourl +'" ><img src="'+data.owner.profile_pic_url+'" class="lml-instagram-avatar-3" /> @'+data.owner.username+
-        '<span class="lml-instagram-via-3">embedded via <i class="fa fa-instagram"> </i> </span> </a> </p>';
+        '<span class="lml-instagram-via-3">embedded via <i class="fab fa-instagram">&nbsp;</i> </span> </a> </p>';
 }
 
 var handleRequest = function(cli) {
