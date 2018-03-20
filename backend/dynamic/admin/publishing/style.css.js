1b1864f8e7c03e156e6acf1065443382eb345216
Erik Desjardins <erik.desjardins@gmail.com>
2018-03-20 01:01:04 -0400

    Frontend unpublish workflow

--- backend/dynamic/admin/publishing/style.css.js
+++ backend/dynamic/admin/publishing/style.css.js
@@ -6,12 +6,6 @@ main {
 #sticky-actions button {
     display: block;
     padding: 8px 14px;
-    background-color: #Af57E4;
-    color: #FFF;
-    text-transform: uppercase;
-    font-weight: bold;
-    border-bottom: 3px solid #793b9e;
-    cursor: pointer;
     box-shadow: 0px 0px 1px 1px #c260ff inset;
     border-radius: 4px;
     width: 100%;
@@ -55,6 +49,13 @@ body #lilium .liliumtext.theme-minim.fullscreen {
     box-shadow: 0px 0px 3px #a6ffb0;
 }
 
+.history-card.history-card-unpublished {
+    background-color: #f0dbc4;
+    background-image: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23caa4a4' fill-opacity='0.4' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E");
+    border: 1px solid #d0cbb4;
+    box-shadow: 0px 0px 3px #f3a24a;
+}
+
 #created-history-card {
     background-color: #e6e1ee;
     background-image: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23cfbaf2' fill-opacity='0.4' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E");
@@ -79,9 +80,7 @@ body #lilium .liliumtext.theme-minim.fullscreen {
     background: #FFF;
     padding: 10px;
     font-size: 14px;
-}
-
-.history-card {
+    border-radius: 3px;
     margin-bottom: 10px;
 }
 
