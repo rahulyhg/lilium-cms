e07c7f3e98aa7ff249d244d9e961691c739949a6
Erik Desjardins <erik.desjardins@gmail.com>
2018-03-22 01:33:39 -0400

    Fixed wrong button names with camelcase

--- backend/dynamic/admin/publishing/style.css.js
+++ backend/dynamic/admin/publishing/style.css.js
@@ -89,6 +89,7 @@ body #lilium .liliumtext.theme-minim.fullscreen {
     font-size: 14px;
     border-radius: 3px;
     margin-bottom: 10px;
+    box-shadow: 0px 0px 3px rgba(0,0,0,0.2);
 }
 
 #publishing-actions {
