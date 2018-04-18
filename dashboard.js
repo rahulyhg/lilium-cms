2d38642f43e0abe078577ef940e70a1cc043b78d
Erik Desjardins <erik.desjardins@gmail.com>
2018-04-18 00:46:50 -0400

    Dashboard convenience method for report

--- dashboard.js
+++ dashboard.js
@@ -37,29 +37,33 @@ class Dashboard {
         });
     };
 
+    getQuote(send) {
+        sharedcache.get('quoteoftheday', quote => {
+            if (quote && quote.day == new Date().getDate()) {
+                send(quote);
+            } else {
+                request('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', {json:true}, (err, resp, text) => {
+                    try {
+                        if (typeof text == "string") {
+                            text = JSON.parse(text);
+                        }
+
+                        quote = text;
+                        quote.day = new Date().getDate();
+                        sharedcache.set({ quoteoftheday : quote });
+
+                        send(quote);
+                    } catch (err) {
+                        send({});
+                    }
+                });
+            }
+        });
+    }
+
     livevar(cli, levels, params, send) {
         if (levels[0] == "quote") {
-            sharedcache.get('quoteoftheday', quote => {
-                if (quote && quote.day == new Date().getDate()) {
-                    send(quote);
-                } else {
-                    request('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en', {json:true}, (err, resp, text) => {
-                        try {
-                            if (typeof text == "string") {
-                                text = JSON.parse(text);
-                            }
-    
-                            quote = text;
-                            quote.day = new Date().getDate();
-                            sharedcache.set({ quoteoftheday : quote });
-
-                            send(quote);
-                        } catch (err) {
-                            send({});
-                        }
-                    });
-                }
-            });
+            this.getQuote(send);
         } else {
             send({});
         }
