91681d94f9ef569904ddebb3b55891f7e699a848
Erik <erik.desjardins@gmail.com>
2018-04-26 14:29:38 -0400

    Fixed TDL

--- thedailylilium.js
+++ thedailylilium.js
@@ -235,7 +235,7 @@ class TheDailyLilium {
                         report.customposts = customposts;
                         sendback(report);
                     }
-                }
+                });
             });
         }
     }
