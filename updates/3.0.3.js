af284bd82c540af2e8c0d8866331ca4d06fcaf95
Erik Desjardins <erik.desjardins@gmail.com>
2018-05-01 20:34:48 -0400

    Ads update script for 3.0.3

---
+++ updates/3.0.3.js
@@ -0,0 +1,27 @@
+const db = require('../includes/db');
+
+module.exports = (_c, done) => {
+    db.find(_c, 'ads', {}, [], (err, cur) => {
+        cur.sort({ _id : -1 }).toArray((err, allads) => {
+            const langs = {
+                en : { content : [], amp : [] },
+                fr : { content : [], amp : [] }
+            };  
+
+            allads.forEach(x => {
+                langs[x.lang][x.type].push({markup : x.markup});
+            });
+
+            db.remove(_c, 'ads', {}, () => {
+                db.insert(_c, 'ads', [
+                    { format : "adset", lang : "en", type : "content", ads : langs.en.content },
+                    { format : "adset", lang : "fr", type : "content", ads : langs.fr.content },
+                    { format : "adset", lang : "en", type : "amp", ads : langs.en.amp },
+                    { format : "adset", lang : "fr", type : "amp", ads : langs.fr.amp }
+                ], () => {
+                    done();
+                });
+            });
+        });
+    });
+};
