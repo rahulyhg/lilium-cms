6a1dc71b944dd1447e9950a9c1ec861376ccb5bb
Erik <erik.desjardins@gmail.com>
2018-05-06 17:49:45 -0400

    Added sidebar history style for "slug"

--- backend/dynamic/admin/publishing/style.css.js
+++ backend/dynamic/admin/publishing/style.css.js
@@ -97,6 +97,11 @@ body #lilium .liliumtext.theme-minim.fullscreen {
     box-shadow: 0px 0px 3px #c9a7ff;
 }
 
+.history-card.history-card-slug {
+    background-color: #d8f0e2;
+    background-image: url("data:image/svg+xml,%3Csvg width='20' height='20' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23e8fff9' fill-opacity='0.4' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='3'/%3E%3Ccircle cx='13' cy='13' r='3'/%3E%3C/g%3E%3C/svg%3E");
+}
+
 .history-card span {
     padding-left: 10px;
 }
