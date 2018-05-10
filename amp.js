b034bbb80cee4949d5d6b350b23ae688d7cda7f2
Erik Desjardins <erik.desjardins@gmail.com>
2018-05-10 00:24:47 -0400

    AMP hooks are now per site

--- amp.js
+++ amp.js
@@ -211,7 +211,7 @@ class Amp {
 
                 db.findUnique(cli._c, 'ads', { type : "amp", lang }, (err, adset) => {
                     const ads = adset.ads;
-                    hooks.fire("amp_replace_ads_" + cli._c.uid, { article : articlewrap, dom : dom, theme : cTheme, lang, ads });
+                    hooks.fireSite(cli._c, "amp_replace_ads", { article : articlewrap, dom : dom, theme : cTheme, lang, ads });
                     articleContent = articlewrap.content;
                     articleContent = articleContent.replace('<lml-related></lml-related>', '').replace(/style=/g, "amp-style=");
 
