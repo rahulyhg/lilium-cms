cc09b4e9382ac814b9612ad46ca7589312e74592
Erik Desjardins <erik.desjardins@gmail.com>
2018-05-13 14:41:01 -0400

    Created touchstone file to help detect heavy libs

---
+++ touchstone.js
@@ -0,0 +1,77 @@
+const log = require('./log');
+global.liliumenv = {
+    mode : "script"
+};
+
+const clearCache = () => {
+    Object.keys(require.cache).forEach(file => !require.resolve(file).endsWith('.node') && delete require.cache[file]);
+}
+
+const thirdparties = [
+    "jsdom", "request", "googleapis", "busboy", "canvas", "deep-diff", "diff", "facebook-ads-sdk",
+    "franc-min", "js-beautify", "mongodb", "mkdirp", "readdirp", "redis-server",
+    "retext", "slugify", "socket.io", "socket.io-redis", "stackpath", "websocket", "ws",
+    'retext', 'retext-diacritics', 'retext-repeated-words', 'retext-spell', 'retext-keywords',
+    'retext-syntax-mentions', 'retext-overuse', 'retext-syntax-urls'
+];
+
+const libLoadTimes = [];
+const lrequire = file => {
+    clearCache();
+
+    const now = Date.now();
+    const lib = require(file);
+    const ttload = Date.now() - now;
+
+    libLoadTimes.push({ file, ttload, lib });
+
+    return lib;
+};
+
+log('ThirdParties', 'Loading a list of ' + thirdparties.length + ' libraries', 'info');
+thirdparties.forEach(file => {
+    lrequire(file);
+});
+
+libLoadTimes.sort((a, b) => b.ttload - a.ttload).forEach(lib => lib.ttload > 50 && log('LibLoadTime', lib.ttload + "ms to load " + lib.file, 'warn'));
+
+
+console.log();
+console.log();
+log('Libs', 'Loading all Lilium libraries', 'info');
+const loadTimes = [];
+const rrequire = file => {
+    clearCache();
+
+    const now = Date.now();
+    const lib = require(file);
+    const ttload = Date.now() - now;
+
+    loadTimes.push({ file, ttload, lib });
+
+    return lib;
+};
+
+const fs = require('fs');
+const exclusion = ["gardener.js", "runscript.js", "index.prod.js", "masthead.js", "touchstone.js"];
+const corefiles = fs.readdirSync(".").filter(x => x.endsWith('.js') && !exclusion.includes(x)).map(x => "./" + x); 
+corefiles.forEach(file => {
+    rrequire(file);
+});
+
+loadTimes.sort((a, b) => b.ttload - a.ttload).forEach(lib => lib.ttload > 500 && log('LoadTime', lib.ttload + "ms to load " + lib.file, 'warn'));
+
+
+
+console.log();
+console.log();
+log('Lilium', 'Computing the total load time', 'info');
+clearCache();
+
+const corenow = Date.now();
+corefiles.forEach(file => require(file));
+
+const initTime = Date.now() - corenow;
+log('Lilium', 'Total load time for loading everything is ' + initTime + 'ms', 'lilium');
+
+
