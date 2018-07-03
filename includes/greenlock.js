10090a9676f6db26a3ed8efda53b9ece6522f165
anon <erik.desjardins@gmail.com>
2018-07-03 14:50:53 -0400

    Greenlock integration with full cert management

--- includes/greenlock.js
+++ includes/greenlock.js
@@ -1,36 +1,43 @@
 const Greenlock = require('greenlock');
+const fs = require('fs');
+const path = require('path');
 
 class GreenlockWrapper {
-    generateCert(_c, done) {
+    generateCert(_c, done, skipVariation) {
         const curedURL = _c.server.url.substring(2);
         const levels = curedURL.split('.');
         const urlVariation = levels.length > 2 ? levels.splice(1).join('.') : ("www." + levels.join('.'));
 
         const options = {
-            domains : [curedURL, urlVariation],
+            domains : skipVariation ? [curedURL] : [curedURL, urlVariation],
             email : _c.emails.senderemail,
-            agreeTos : true
+            agreeTos : true,
+            skipChallengeTest : true
         }
 
         const greenlock = Greenlock.create({
             version: 'draft-11',
             server: _c.env == "dev" ? "https://acme-staging-v02.api.letsencrypt.org/directory" : 'https://acme-v02.api.letsencrypt.org/directory',
-            webrootPath: _c.server.html
+            webrootPath: _c.server.html + "/.well-known/acme-challenge/",
+            skipChallengeTest : true
         });
         
+        console.log(options);
         greenlock.register(options).then(certs => {
             log('Greenlock', 'Generated cert successfully', 'success');
-            console.log(certs);
+            log('Greenlock', 'Will expire at ' + certs.expiredAt, 'info');
             // privkey, cert, chain, expiresAt, issuedAt, subject, altnames
+
+            log('Greenlock', 'Writing files to keys directory', 'info');
+            fs.writeFileSync(path.join(_c.server.base, '..', 'keys', 'default_fullchain.pem'), `${certs.cert}${certs.chain}`.replace(/\r/g, ''));
+            fs.writeFileSync(path.join(_c.server.base, '..', 'keys', 'default_privkey.pem'), certs.privkey);
+            log('Greenlock', 'Wrote HTTPS certs in pem files inside keys directory', 'success');
+
             done(true);
         }, err => {
             log('Greenlock', err.toString(), 'err');
             done(false);
         });
-    }
-
-    updateNginx(_c, done) {
-
     }
 }
 
