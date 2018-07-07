dce7bd8816e367cd26d5815370e888e49ab7655c
anon <erik.desjardins@gmail.com>
2018-07-07 16:32:46 -0400

    Lilium Preact app Index routing + cred validation

--- apps/lilium/index.html.js
+++ apps/lilium/index.html.js
@@ -3,6 +3,8 @@ module.exports = cli => `<!DOCTYPE html>
     <head>
         <title>Lilium | ${cli._c.website.sitetitle}</title>
         <link rel="icon" type="image/png" href="${cli._c.server.protocol}${cli._c.server.url}/static/media/favicon.png">
+        <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="${cli._c.server.protocol}${cli._c.server.url}/lilium.css" />
     </head>
     <body>
         <div id="app"></div>
