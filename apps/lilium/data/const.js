810c90307c43b4880904bd4d17231dd02d7d0009
anon <erik.desjardins@gmail.com>
2018-07-15 03:04:38 -0400

    Timings will be stored in const repo

--- apps/lilium/data/const.js
+++ apps/lilium/data/const.js
@@ -3,6 +3,10 @@ export const LILIUM = {
     v : 4.0
 }
 
+export const TIMING = {
+    LYS_QUICK_SEARCH : 100
+}
+
 export const POST_STATUS = {
     published : { w : "Published" },
     deleted   : { w : "Back to draft" },
