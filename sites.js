641ea23c3d67ba2bc60617f4532af9a906dd793b
anon <erik.desjardins@gmail.com>
2018-07-15 03:04:55 -0400

    Lilium V4 compilation logic will include CSS

--- sites.js
+++ sites.js
@@ -265,12 +265,12 @@ var SiteInitializer = function (conf, siteobj) {
                     };
                 `;
             }
+            
+            fs.copyFileSync(
+                pathLib.join(conf.server.base, 'apps', 'lilium', 'App.css'),
+                pathLib.join(conf.server.html, 'lilium.css')
+            );
         });
-
-        fs.copyFileSync(
-            pathLib.join(conf.server.base, 'apps', 'lilium', 'App.css'),
-            pathLib.join(conf.server.html, 'lilium.css')
-        );
 
         hooks.fireSite(conf, 'frontend_will_precompile', {
             config: conf,
