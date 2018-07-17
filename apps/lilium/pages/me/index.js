4f88eabedb2c3ac4ab1c9422777114ecc4f5ec70
Gabriel Cardinal <gabrielc@outlook.fr>
2018-07-17 14:51:06 -0400

    Implemented API call on profile page to fetch user

--- apps/lilium/pages/me/index.js
+++ apps/lilium/pages/me/index.js
@@ -1,13 +1,41 @@
 import { h, Component } from "preact";
+import API from '../../data/api';
 
 export default class ProfilePage extends Component {
     constructor(props) {
         super(props);
+
+        this.state = { user: undefined, err: undefined };
+    }
+
+    componentWillMount() {
+        API.get('/entities/me', {}, (err, user) => {
+            if (!err && user) {
+                this.setState({ user });
+                console.log(this.state);
+            } else {
+                console.log(err);
+                this.setState({ err });x
+            }
+        });
     }
 
     render() {
-        return (
-            <h1>PROFILE PAGE</h1>
-        );
+        if (!this.state.err && this.state.user) {
+            return (
+                <div id="profile-header">
+                    <img src="" />
+                    <h3 id="user-name">{this.state.user.username}</h3>
+                    <h4 id="first-name">{this.state.user.firstname}</h4>
+                    <h4 id="last-name">{this.state.user.lastname}</h4>
+                </div>
+            );   
+        } else {
+            return (
+                <div id="profile-error">
+                    <h3>There was an error when trying to retrieve your user info, are you logged in?</h3>
+                </div>
+            );
+        }
     }
 }
