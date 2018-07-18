eaabf9f3fb2c01acc5000d72d2fa36fa56eb5c64
Gabriel Cardinal <gabrielc@outlook.fr>
2018-07-18 11:35:02 -0400

    Styled profile image on profile page

--- apps/lilium/pages/me/index.js
+++ apps/lilium/pages/me/index.js
@@ -1,7 +1,35 @@
 import { h, Component } from "preact";
 import API from '../../data/api';
 
+const style = {
+    header: {
+        backgroundColor: '#2D153B',
+        color: 'white',
+        padding: '25px',
+        display: 'flex',
+        fontWeight: 'light',
+        flexWrap: 'nowrap'
+    },
+
+    profilePicture: {
+        width: '250px',
+        height: '250px',
+        objectFit: 'cover',
+        border: '2px solid white',
+        borderRadius: '50%',
+        backgroundColor: '#333',
+        overflow: 'hidden'
+    },
+
+    profileInfoWrapper: {
+        marginLeft: '40px',
+        borderLeft: '1px solid white',
+        padding: '0px 30px'
+    }
+};
+
 export default class ProfilePage extends Component {
+
     constructor(props) {
         super(props);
 
@@ -11,8 +39,8 @@ export default class ProfilePage extends Component {
     componentWillMount() {
         API.get('/entities/me', {}, (err, user) => {
             if (!err && user) {
-                this.setState({ user });
-                console.log(this.state);
+                console.log(user);
+                this.setState({ user: user.user, err: undefined });
             } else {
                 console.log(err);
                 this.setState({ err });x
@@ -21,15 +49,19 @@ export default class ProfilePage extends Component {
     }
 
     render() {
-        if (!this.state.err && this.state.user) {
+        if (this.state.user) {
             return (
-                <div id="profile-header">
-                    <img src="" />
-                    <h3 id="user-name">{this.state.user.username}</h3>
-                    <h4 id="first-name">{this.state.user.firstname}</h4>
-                    <h4 id="last-name">{this.state.user.lastname}</h4>
+                <div id="profile-header" style={style.header}>
+                    <div id="profile-picture-wrapper">
+                        <img src={this.state.user.avatarURL} id="profile-picture" style={style.profilePicture} />
+                    </div>
+                    <div id="profile-info-wrapper" style={style.profileInfoWrapper}>
+                        <h1 id="user-name">{this.state.user.username}</h1>
+                        <h1 id="first-name">{this.state.user.firstname}</h1>
+                        <h1 id="last-name">{this.state.user.lastname}</h1>
+                    </div>
                 </div>
-            );   
+            );
         } else {
             return (
                 <div id="profile-error">
