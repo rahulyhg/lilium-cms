e8a91ad74e788ee9243cc5e98a0bde9518347cb2
anon <erik.desjardins@gmail.com>
2018-07-20 01:40:45 -0400

    Menu layout with x-axis parallax

--- apps/lilium/layout/menu.js
+++ apps/lilium/layout/menu.js
@@ -17,25 +17,41 @@ export class LiliumMenu extends Component {
         }
     }    
 
+    focusedChanged(slid) {
+        this.setState({ focused : slid });
+        const ev = new CustomEvent("menuslid", { detail : { slid } });
+        document.dispatchEvent(ev);
+    }
+
+    mousehover(ev) {
+        this.focusedChanged(true);        
+    }
+
+    mouseleave(ev) {
+        this.focusedChanged(false);        
+    }
+
     render() {
+        const links = [];
+        this.state.menus.forEach(menu => {
+            links.push(
+                (<div class="menu-item"><Link linkStyle="block" href={menu.absURL.replace('admin', '')}>
+                    <i className={"fa " + menu.faicon}></i> <span>{menu.displayname}</span>
+                </Link></div>)
+            );
+
+            menu.children.map(child => {
+                links.push(
+                    (<div class="menu-item"><Link linkStyle="block" href={child.absURL.replace('admin', '')}>
+                        <i className={"fa " + child.faicon}></i> <span>{child.displayname}</span>
+                    </Link></div>)
+                );
+            });
+        });        
+
         return (
-            <menu id="lilium-menu">
-                {
-                    this.state.menus.map(menu => (
-                        <div>
-                            <Link display="block" href={menu.absURL.replace('admin', '')}>
-                                <i className={"fa " + menu.faicon}></i> <span>{menu.displayname}</span>
-                            </Link>
-                            <div>
-                                {menu.children.map(child => (
-                                    <Link display="block" href={child.absURL.replace('admin', '')}>
-                                        <i className={"fa " + child.faicon}></i> <span>{child.displayname}</span>
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
-                    ))
-                }
+            <menu id="lilium-menu" onMouseEnter={this.mousehover.bind(this)} onMouseLeave={this.mouseleave.bind(this)} style={ this.state.focused ? { transform: "translate3d(0,0,0)" } : {} }>
+                { links }
             </menu>
         )
     }
