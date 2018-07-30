713c25b2cbf7a9d6c11852281e9bc949510b3b0e
anon <erik.desjardins@gmail.com>
2018-07-30 12:48:01 -0400

    Entities will accept avatarURL as field

--- entities.js
+++ entities.js
@@ -49,7 +49,7 @@ const SOCIAL_NETWORKS = {
 
 const ALLOWED_ME_FIELDS = [ 
     // Basic information
-    "displayname", "description", "email", "phone", "personality", 'jobtitle',
+    "displayname", "description", "email", "phone", "personality", 'jobtitle', 'avatarURL',
 
     // Social profiles
     "socialnetworks.facebook", "socialnetworks.twitter", "socialnetworks.instagram", "socialnetworks.googleplus"
