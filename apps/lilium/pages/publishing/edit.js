a80a7e5611c3cd2d8459b37a92c8409acbd3677f
anon <erik.desjardins@gmail.com>
2018-08-08 13:18:18 -0400

    Full history bar support with color codes

--- apps/lilium/pages/publishing/edit.js
+++ apps/lilium/pages/publishing/edit.js
@@ -4,6 +4,7 @@ import { setPageCommands } from '../../layout/lys';
 import { TextEditor } from '../../widgets/texteditor';
 import { TextField, ButtonWorker } from '../../widgets/form';
 import { getSession } from '../../data/cache';
+import { castNotification } from '../../layout/notifications';
 
 import dateFormat from 'dateformat';
 
@@ -23,10 +24,6 @@ const styles = {
 
 
 class HistoryEntry extends Component {
-    makeBackgroundStyle() {
-
-    }
-
     createMessage() {
         switch (this.props.entry.type) {
             case "update":
@@ -34,7 +31,7 @@ class HistoryEntry extends Component {
             case "published":
                 return "published this article";
             case "unpublished":
-                return "set back this article to draft";
+                return "set this article back to draft";
             case "submitted":
                 return "sent this article for review";
             case "refused":
@@ -50,7 +47,7 @@ class HistoryEntry extends Component {
 
     render() {
         return (
-            <div class={"history-entry history-entry-" + this.props.entry.type}>
+            <div key={this.props.key} class={"history-entry history-entry-" + this.props.entry.type}>
                 <div class="history-entry-avatar-wrap">
                     <img class="history-entry-avatar" src={this.props.actor.avatarURL} />
                 </div>
@@ -68,7 +65,7 @@ class PublishingHistory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            history : []
+            history : props.history || []
         };
 
         const cachedUsers = {};
@@ -78,21 +75,17 @@ class PublishingHistory extends Component {
         this.cachedUsers = cachedUsers;
     }
 
-    refresh() {
-        API.get('/publishing/history/' + this.props.post._id, {}, (err, history) => {
-            this.setState({ history });
-        })
-    }
-
-    componentDidMount() {
-        this.refresh();
+    componentWillReceiveProps(props) {
+        log('History', 'History widget received props, about to set state', 'detail');
+        this.setState({ history : props.history });
     }
     
     render() {
+        log('History', 'Rendering history with ' + this.state.history.length + ' entries', 'detail');
         return (
             <div>
                 {
-                    this.state.history.map(entry => (<HistoryEntry entry={entry} actor={this.cachedUsers[entry.actor]} />))
+                    this.state.history.map(entry => (<HistoryEntry key={entry._id} entry={entry} actor={this.cachedUsers[entry.actor]} />))
                 }
 
                 <HistoryEntry entry={{
@@ -108,12 +101,20 @@ class PublishingSidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            post : props.post
+            post : props.post,
+            actions : props.actions || [],
+            history : props.history || []
         }
     }
 
     componentWillReceiveProps(props) {
-        this.setState({ post : props.post });
+        const newState = {};
+
+        props.history && (newState.history = props.history);
+        props.post && (newState.post = props.post);
+        props.actions && (newState.actions = props.actions);
+
+        this.setState(newState);
     }
 
     render() {
@@ -125,11 +126,11 @@ class PublishingSidebar extends Component {
             <div>
                 <b style={styles.sidebarTitle}>Manage</b>
                 <div style={{ padding : 10 }}>
-                    <ButtonWorker text="Publish" />
+                    {this.state.actions}
                 </div>
 
                 <b style={styles.sidebarTitle}>Activity</b>
-                <PublishingHistory post={this.state.post} />
+                <PublishingHistory post={this.state.post} history={this.state.history} />
             </div>
         );
     }
@@ -141,21 +142,77 @@ export default class EditView extends Component {
 
         this.state = {
             loading : true,
-            actions : []
+            actions : [],
+            lastEdit : undefined
         };
 
         this.coldState = {};
+        this.edits = {};
     }
 
     componentDidMount() {
         this.requestArticle(this.props.postid);
     }
 
-    save() {
-        log('Publishing', 'Saving current post with id ' + this.props.postid, 'detail');
+    save(done) {
+        const fieldCount = Object.keys(this.edits).length;
+
+        if (fieldCount == 0) {
+            castNotification({
+                type : "info",
+                title : "No modification",
+                message : "This version of the article is the same as the one in the database."
+            })
+
+            done && done();
+        } else {
+            log('Publishing', 'Saving current post with id ' + this.props.postid, 'detail');
+
+            API.put('/publishing/save/' + this.coldState.post._id, this.edits, (err, json, r) => {
+                if (json && json.historyentry) {            
+                    log('Publishing', 'Article saved!', 'success');
+
+                    castNotification({
+                        type : "success",
+                        title : "Article saved",
+                        message : "This article was successfully saved in the database."
+                    })
+
+                    this.setState({
+                        history : [json.historyentry, ...this.state.history],
+                        post : {...this.state.post, ...this.edits}
+                    }, () => {
+                        this.edits = {};
+                        this.coldState.post = this.state.post;
+
+                        done && done();
+                    });
+                } else {
+                    castNotification({
+                        type : "warn",
+                        title : "Article did not save",
+                        message : `[${r.status}] Error message from Lilium.`
+                    })
+
+                    done && done();
+                }
+            });
+        }
     }
 
-    requestArticle(postid) {
+    publish(done) {
+
+    }
+
+    preview(done) {
+
+    }
+
+    commitchanges(done) {
+
+    }
+
+    requestArticle(postid, done) {
         this.setState({ loading : true });
         setPageCommands([{
             command : "save",
@@ -164,7 +221,8 @@ export default class EditView extends Component {
         }]);
 
         const endpoints = {
-            post : { endpoint : "/publishing/write/" + postid, params : {} }
+            post : { endpoint : "/publishing/write/" + postid, params : {} },
+            history : { endpoint : "/publishing/history/" + postid, params : {} }
         };
 
         API.getMany(Object.keys(endpoints).map(key => endpoints[key]), resp => {
@@ -174,10 +232,27 @@ export default class EditView extends Component {
                 log('Publishing', 'Article not found : ' + postid, 'warn');
             
             this.coldState.post = post;
-            this.setState(post ? { post, loading: false } : { error : "Article not Found", loading : false }, () => {
-                
+            this.setState(post ? { post, loading: false, actions : this.getActionFromColdState(), history : resp[endpoints.history.endpoint] } : { error : "Article not Found", loading : false }, () => {
+                done && done();
             });
         });
+    }
+
+    getActionFromColdState() {
+        const status = this.coldState.post.status;
+        const actions = [<ButtonWorker text="Preview" work={this.preview.bind(this)} />];
+
+        if (status == "draft" || status == "deleted") {
+            actions.push(<ButtonWorker text="Save" work={this.save.bind(this)} />, <ButtonWorker text="Publish" work={this.publish.bind(this)} />);
+        } else if (status == "published") {
+            actions.push(<ButtonWorker text="Save" work={this.save.bind(this)} />, <ButtonWorker text="Commit changes" work={this.commitchanges.bind(this)} />);
+        } 
+
+        return actions;
+    }
+
+    fieldChanged(name, value) {
+        this.edits[name] = value;
     }
 
     componentWillReceiveProps(props) {
@@ -204,16 +279,16 @@ export default class EditView extends Component {
         return (
             <div>
                 <div style={{ padding : 20, marginRight: 280 }}>
-                    <TextField initialValue={this.state.post.title[0]} placeholder="Publication headline" placeholderType="inside" wrapstyle={{ marginBottom: 6 }} style={{ fontFamily : '"Oswald", sans-serif', background : "transparent", fontSize : 32, border : "none", borderBottom : "1px solid #DDD", padding : "3px 0px", textAlign : 'center' }} />
-                    <TextField initialValue={this.state.post.subtitle[0]} placeholder="Subtitle or catchline" placeholderType="inside" style={{ background : "transparent", border : "none", padding : "5px 0px", marginBottom : 10, textAlign : 'center' }} />
+                    <TextField onChange={this.fieldChanged.bind(this)} format={x => [x]} name="title" initialValue={this.state.post.title[0]} placeholder="Publication headline" placeholderType="inside" wrapstyle={{ marginBottom: 6 }} style={{ fontFamily : '"Oswald", sans-serif', background : "transparent", fontSize : 32, border : "none", borderBottom : "1px solid #DDD", padding : "3px 0px", textAlign : 'center' }} />
+                    <TextField onChange={this.fieldChanged.bind(this)} format={x => [x]} name="subtitle" initialValue={this.state.post.subtitle[0]} placeholder="Subtitle or catchline" placeholderType="inside" style={{ background : "transparent", border : "none", padding : "5px 0px", marginBottom : 10, textAlign : 'center' }} />
                     
                     <div style={{ maxWidth: 800, margin: "auto" }}>
-                        <TextEditor content={this.state.post.content[0]}></TextEditor>
+                        <TextEditor onChange={this.fieldChanged.bind(this)} format={x => [x]} name="content" content={this.state.post.content[0]} />
                     </div>
                 </div>
 
                 <div style={{ position : "fixed", right : 0, top : 50, bottom : 0, width : 280, overflow : "auto", background : "rgb(238, 226, 241)", boxShadow : "1px 0px 2px 2px rgba(154, 145, 156, 0.46)" }}>
-                    <PublishingSidebar post={this.state.post} actions={this.state.actions}  />
+                    <PublishingSidebar post={this.state.post} actions={this.state.actions} history={this.state.history} />
                 </div>
             </div>
         )
