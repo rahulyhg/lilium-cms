715a318ff8919322548e46884222a4c3260eee27
anon <erik.desjardins@gmail.com>
2018-08-14 16:58:09 -0400

    Theme settings will render form based on info.json

--- apps/lilium/pages/themes/index.js
+++ apps/lilium/pages/themes/index.js
@@ -1,5 +1,52 @@
 import { h, Component } from 'preact';
 import API from '../../data/api';
+import { TextField, SelectField } from '../../widgets/form';
+
+class ThemeSettingsForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme : props.theme
+        };
+
+        this.coldState = props.theme.settings || {};
+    }
+
+    componentWillReceiveProps(props) {
+        this.setState({ theme : props.theme });
+    }
+
+    componentDidMount() {
+
+    }
+
+    fieldFromInfoEntry(name, entry) {
+        switch (entry.type) {
+            case "select": return (<SelectField 
+                placeholder={entry.attr.displayname} 
+                initialValue={this.coldState[name]} 
+                options={entry.attr.datasource.map(s => { return { displayname : s.displayName, value : s.name } })}
+            />);
+
+            case "text": default: return (<TextField 
+                placeholder={entry.attr.displayname} 
+                initialValue={this.coldState[name]} 
+            />);
+        }
+    }
+
+    render() {
+        return (
+            <div class="theme-settings-form">
+                {Object.keys(this.state.theme.settingForm).map(fieldname => {
+                    const FieldComponent = this.fieldFromInfoEntry(fieldname, this.state.theme.settingForm[fieldname]);
+
+                    return FieldComponent;
+                })}
+            </div>
+        );
+    }
+}
 
 class ThemeCard extends Component {
     constructor(props) {
@@ -55,6 +102,10 @@ export default class ThemeSettings extends Component {
                     {this.state.themes.map(x => (
                         <ThemeCard theme={x} active={x.uName == this.state.current.uName} />
                     ))}
+                </div>
+                <h1>Settings</h1>
+                <div class="theme-settings">
+                    <ThemeSettingsForm theme={this.state.current} />
                 </div>
             </div>
         );
