a2a6a284dcb75295393dfd807d81996766932b81
Erik Desjardins <erik.desjardins@gmail.com>
2018-08-13 22:55:59 -0400

    Livevar endpoint for listing themes + active one

--- apps/lilium/pages/themes/index.js
+++ apps/lilium/pages/themes/index.js
@@ -1,14 +1,38 @@
 import { h, Component } from 'preact';
+import API from '../../data/api';
 
 export default class ThemeSettings extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            ready : false
+        }
+    }
+
+    componentDidMount() {
+        API.getMany([
+            { endpoint : "/themes/all" },
+            { endpoint : "/themes/current" }
+        ], (err, resp) => {
+            this.setState({ themes : resp["/themes/all"], current : resp["/themes/current"], ready : true });
+        })
     }
 
     render() {
+        if (!this.state.ready) {
+            return (<div>Loading</div>);
+        }
+
         return (
             <div id="theme-settings-page">
-
+                <h1>Themes</h1>
+                <div>
+                    {this.state.themes.map(x => (
+                        <div>
+                            {x.dName}
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
