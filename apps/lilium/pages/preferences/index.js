7219af9bc0e1dded58c343c586395b31415f2eb8
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2018-08-14 21:43:50 -0400

    Removed MultiTagBox testing in preferences page

--- apps/lilium/pages/preferences/index.js
+++ apps/lilium/pages/preferences/index.js
@@ -72,7 +72,6 @@ export default class Preferences extends Component {
                                         onChange={this.valueChanged.bind(this)} />
                         <CheckboxField name='badgesNotifications' placeholder='Enable badges popup notifications' initialValue={this.values['badgesNotifications']}
                                         onChange={this.valueChanged.bind(this)} />
-                        <MultitagBox name='tagstestname' initialValue={['a', 'b']} placeholder='Enter some tags!' onChange={(name, val) => { console.log(name, val) }} />
                     </div>
                 </div>
             );
