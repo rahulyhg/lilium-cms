de825def1083dfdc2f726269974df6797c01595c
anon <erik.desjardins@gmail.com>
2018-08-20 15:49:36 -0400

    Cakepop edit screen

--- apps/lilium/pages/cakepops/edit.js
+++ apps/lilium/pages/cakepops/edit.js
@@ -1,7 +1,7 @@
 import { h, Component } from "preact";
 import API from '../../data/api';
 import { Spinner } from '../../layout/loading';
-import { TextField, CheckboxField, SelectField } from '../../widgets/form';
+import { TextField, CheckboxField, SelectField, DatePicker } from '../../widgets/form';
 import { TextEditor } from '../../widgets/texteditor';
 
 const styles = {
@@ -31,9 +31,17 @@ export default class CakepopEditPage extends Component {
                 }); 
             } else {
                 this.setState({
+                    loading : false,
                     error : err || "Not found"
                 });
             }
+        });
+    }
+
+    changed(field, value) {
+        this.coldState[field] = value;
+        API.post('/cakepop/updateOneField/' + this.props.id, { field, value }, () => {
+            log('Cakepop', 'Updated one field : ' + field, 'success');
         });
     }
 
@@ -52,15 +60,16 @@ export default class CakepopEditPage extends Component {
         
         return (
             <div class="cakepop-edit-page">
-                <TextField initialValue={this.coldState.title} placeholderType="inside" style={styles.title} placeholder="Cakepop identifier" />
-                <TextEditor content={this.coldState.content} />
-                <TextField multiline={true} initialValue={this.coldState.stylesheet} placeholder="Custom Stylesheet" />
-                <CheckboxField initialValue={this.coldState.auto} placeholder="Automatically popup on next reload" />
-                <TextField initialValue={this.coldState.expiry} placeholder="Expiry" type="date" />
-                <SelectField initialValue={this.coldState.status} placeholder="Status" options={[
+                <TextField     onChange={this.changed.bind(this)} name="title"      initialValue={this.coldState.title} placeholderType="inside" style={styles.title} placeholder="Cakepop identifier" />
+                <TextEditor    onChange={this.changed.bind(this)} name="content"    content={this.coldState.content} style={{ marginBottom : 15 }} />
+                <TextField     onChange={this.changed.bind(this)} name="stylesheet" multiline={true} initialValue={this.coldState.stylesheet} placeholder="Custom Stylesheet" />
+                <DatePicker    onChange={this.changed.bind(this)} name="expiry"     initialValue={new Date(this.coldState.expiry)} format={x => x && x.getTime()} placeholder="Expiry" />
+                <SelectField   onChange={this.changed.bind(this)} name="status"     initialValue={this.coldState.status} placeholder="Status" options={[
                     { displayname : "Draft", value : "creation" },
                     { displayname : "Live", value : "live" }
                 ]} />
+
+                <CheckboxField onChange={this.changed.bind(this)} name="auto"       initialValue={this.coldState.auto} placeholder="Automatically popup on next reload" />
             </div>
         )
     }
