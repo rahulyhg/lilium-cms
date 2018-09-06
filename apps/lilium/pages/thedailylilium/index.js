383492dad554df91f3479cbd0d8f174a3873a305
anon <erik.desjardins@gmail.com>
2018-09-05 23:10:50 -0400

    Added The Daily Lilium endpoint + template page

---
+++ apps/lilium/pages/thedailylilium/index.js
@@ -0,0 +1,23 @@
+
+import { h, Component } from 'preact'
+
+export default class TheDailyLilium extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>The Daily Lilium</h1>
+            </div>
+        );
+    }
+}
