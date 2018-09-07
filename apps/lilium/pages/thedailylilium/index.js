e3f31330f59fc2a8a5df4405ffe75f89cbc64120
anon <erik.desjardins@gmail.com>
2018-09-07 16:54:09 -0400

    Less file code base with split logic

--- apps/lilium/pages/thedailylilium/index.js
+++ apps/lilium/pages/thedailylilium/index.js
@@ -2,17 +2,50 @@ import { h, Component } from 'preact'
 import API from '../../data/api';
 import dateformat from 'dateformat';
 
-class TopPost extends Component {
+const TopPost = props => (
+    <div class="tdl-toppost">
+        <img class="tdl-toppost-image" src={props.post.fullmedia} />
+        <h3>{props.post.headline}</h3>
+        <h4>{props.post.subline}</h4>
+        <div>
+            <p style={{ textAlign : 'left' }}>
+                <img src={props.post.authorphoto} style={{ height: 48, width: 48, objectFit : "cover", borderRadius: 10, display: "inline-block", verticalAlign : 'middle', marginRight: 10 }} />
+                Written by <b>{props.post.authorname}</b>, the top post generated <b>{props.post.pageviews}</b> page views on the website.
+            </p>
+        </div>
+    </div>
+);
+
+class YesterdayStats extends Component {
     render() {
         return (
-            <div class="tdl-toppost">
-                <img class="tdl-toppost-image" src={this.props.post.fullmedia} />
-                <h3>{this.props.post.headline}</h3>
-                <h4>{this.props.post.subline}</h4>
+            <div class="tdl-yesterday-stats">
+                
             </div>
         )
     }
 }
+
+const NewspaperHeader = () => (
+    <div>
+        <div id="the-daily-lilium-header">
+            <img src="/static/media/lmllogo.png" id="tdl-lilium-logo" />
+            <img src="/static/media/thedailylilium.png" class="tdl-lilium-header-image" />
+        </div>
+        <div id="the-daily-lilium-subheader">
+            <h2>ILLUSTRATED DAILY LILIUM NEWS | NOW IN COLOUR</h2>
+        </div>
+        <div id="the-daily-lilium-dateline">
+            <b>Est. 2018</b>
+            <b>{dateformat(new Date(), 'dddd, mmmm dd, yyyy')}</b>
+            <b>Number {Math.floor(Date.now() / (1000 * 60 * 60 * 24) % 365)}</b>
+        </div>
+    </div>
+);
+
+const DebugDataDump = props => liliumcms.env == "dev" ? (
+    <pre style={{ textAlign: 'left' }}>{JSON.stringify(props.data, null, 4)}</pre>
+) : null;
 
 export default class TheDailyLilium extends Component {
     static rendererstyle = {
@@ -52,27 +85,19 @@ export default class TheDailyLilium extends Component {
 
         return (
             <div id="the-daily-lilium">
-                <div id="the-daily-lilium-header">
-                    <img src="/static/media/lmllogo.png" id="tdl-lilium-logo" />
-                    <img src="/static/media/thedailylilium.png" class="tdl-lilium-header-image" />
-                </div>
-                <div id="the-daily-lilium-subheader">
-                    <h2>ILLUSTRATED DAILY LILIUM NEWS | NOW IN COLOUR</h2>
-                </div>
-                <div id="the-daily-lilium-dateline">
-                    <b>Est. 2018</b>
-                    <b>{dateformat(new Date(), 'dddd, mmmm dd, yyyy')}</b>
-                    <b>Number {Math.floor(Date.now() / (1000 * 60 * 60 * 24) % 365)}</b>
-                </div>
+                <NewspaperHeader />
+
                 <div id="the-daily-lilium-content">
                     <div class="tdl-col">
                         <TopPost post={this.state.data.toparticle} />
                     </div>
 
                     <div class="tdl-col">
-                        
+                        <YesterdayStats stats={this.state.data.stats} articles={this.state.data.articles} />
                     </div>
                 </div>
+
+                <DebugDataDump data={this.state.data} />
             </div>
         );
     }
