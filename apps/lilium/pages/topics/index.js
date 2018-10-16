90637e61ff9a8ff7182ef6225f7b609d42ba16b1
anon <erik.desjardins@gmail.com>
2018-10-16 16:52:15 -0400

    New Lilium URL at /topics

---
+++ apps/lilium/pages/topics/index.js
@@ -0,0 +1,44 @@
+import { h, Component } from 'preact';
+import { SingleTopic } from './single';
+import { TopicPicker } from '../../widgets/form';
+import API from '../../data/api';
+
+export default class TopicsManagement extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            component : null
+        }
+    }
+    
+    componentDidMount() {
+        this.setPage();
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.levels) {
+            this.setPage();
+        }
+    }
+
+    setPage() {
+        if (this.props.levels.length != 0) {
+            this.setTopicFromID(this.props.levels[0]);
+        } 
+    }
+    
+    setTopicFromID() {
+        this.setState({ loading : true });
+        API.get('/topics', {}, (err, json, r) => {
+
+        });
+    }
+
+    render() {
+        return (
+            <div class="manage-topics-page">
+                <TopicPicker session="manager" />
+            </div>
+        );
+    }
+}
---
+++ apps/lilium/pages/topics/single.js
@@ -0,0 +1,20 @@
+import { h, Component } from 'preact';
+
+export class SingleTopic extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
