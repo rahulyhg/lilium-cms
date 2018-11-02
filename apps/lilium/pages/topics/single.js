c665315923a0c1e75b890305958544b9702bcc54
anon <erik.desjardins@gmail.com>
2018-11-02 02:36:04 -0400

    Topic page will load main fields and theme templates

--- apps/lilium/pages/topics/single.js
+++ apps/lilium/pages/topics/single.js
@@ -1,15 +1,22 @@
 import { h, Component } from 'preact';
+import { TextField } from '../../widgets/form';
+import API from '../../data/api';
 
 export class SingleTopic extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            topic : props.topic
+            topic : props.topic,
+            ready : false
         }
     }
     
     componentDidMount() {
-        
+        API.get("/themes/current", {}, (err, json) => {
+            if (json) {
+                this.setState({ theme : json, ready : true })
+            }
+        });
     }
 
     componentWillReceiveProps(props) {
@@ -17,15 +24,27 @@ export class SingleTopic extends Component {
             this.setState({ topic : props.topic })
         }
     }
+
+    onChange(name, value) {
+        
+    }
     
     render() {
-        if (!this.state.topic) {
+        if (!this.state.ready) {
+            return null;
+        }
+
+        if (this.state.ready && !this.state.topic) {
             return null;
         }
 
         return (
             <div class="single-topic-form">
-                { this.state.topic.displayname }
+                <div>
+                    
+                </div>
+                <TextField initialValue={this.state.topic.displayname} value={ this.state.topic.displayname } id="displayname" onChange={this.onChange.bind(this)} placeholder="Display name" />
+                <TextField initialValue={this.state.topic.slug} value={ this.state.topic.slug } id="slug" onChange={this.onChange.bind(this)} placeholder="URL slug" />
             </div>
         );
     }
