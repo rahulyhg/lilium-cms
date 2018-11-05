abf82987064aaec8bfe1a44ac3128024651b7027
anon <erik.desjardins@gmail.com>
2018-11-04 21:53:37 -0500

    Tree will recursively search for selected value, collapse

--- apps/lilium/widgets/treeview.js
+++ apps/lilium/widgets/treeview.js
@@ -9,13 +9,19 @@ class TreeNode extends Component {
         };
     }
 
+    componentWillReceiveProps(props) {
+        if (typeof props.open != "undefined") {
+            this.setState({ open : props.open });
+        }
+    }
+
     toggle() {
         this.setState({ open : !this.state.open });
     }
 
     onClick() {
         this.setState({ open : true });
-        this.props.onEvent("selected", this.props._id);
+        this.props.onEvent("selected", { _id : this.props._id, isRoot : this.props.isRoot });
     }
 
     render() {
@@ -28,7 +34,7 @@ class TreeNode extends Component {
                     </span>
                 </div>
                 <div class="tree-node-children-wrapper">
-                    <TreeNodeCollection onEvent={this.props.onEvent.bind(this)} open={this.state.open} nodes={this.state.children} displayname={this.props.displayname} />
+                    <TreeNodeCollection open={this.state.open} onEvent={this.props.onEvent} open={this.state.open} nodes={this.state.children} displayname={this.props.displayname} _id={this.props._id} isRoot={this.props.isRoot} />
                 </div>
             </div>
         )
@@ -58,9 +64,9 @@ class TreeNodeCollection extends Component {
         return (
             <div class="tree-node-collection">
                 <div class="tree-node-children-list">
-                    { this.state.children.map(x => (<TreeNode onEvent={this.props.onEvent.bind(this)} displayname={x.displayname} children={x.children || []} _id={x._id} />)) }
+                    { this.state.children.map(x => (<TreeNode onEvent={this.props.onEvent} displayname={x.displayname} children={x.children || []} _id={x._id} open={x.open} />)) }
                 </div>
-                <div class="tree-node-add-child" onClick={this.props.onEvent.bind(this, 'addChildren', this.props._id)}>
+                <div class="tree-node-add-child" onClick={this.props.onEvent.bind(this, 'addto', { _id : this.props._id, isRoot : this.props.isRoot })}>
                     <i class="far fa-plus"></i> 
                     <span>Add to {this.props.displayname}</span>
                 </div>
@@ -73,18 +79,52 @@ export class TreeView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nodes : this.props.nodes || []
+            nodes : this.props.nodes || [],
+            value : this.props.value || undefined
         };
     }
 
     componentWillReceiveProps(props) {
+        const newState = {};
         if (props.nodes) {
-            this.setState({ nodes : props.nodes });
+            newState.nodes = props.nodes;
+        } 
+
+        this.setState(newState, () => {
+            if (props.value) {
+                this.updateSelected(props.value);
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.updateSelected(this.state.value);
+    }
+
+    updateSelected(value) {
+        const nodes = this.state.nodes || [];
+        if (value) {
+            const searchChildrenNodes = arr => {
+                const maybeFound = arr.find(x => x._id == value || searchChildrenNodes(x.children || []));
+                if (maybeFound) {
+                    console.log('Found : ' + maybeFound._id);
+                    maybeFound.open = true;
+                    return true;
+                }
+            };
+
+            const maybeFound = nodes.find(node => searchChildrenNodes(node.children || []));
+            if (maybeFound) {
+                console.log('Found : ' + maybeFound._id);
+                maybeFound.open = true;
+
+                this.setState({ nodes })
+            }
         }
     }
 
-    onEvent(name, extra) {
-        this.props.onEvent && this.props.onEvent(name, extra);
+    onEvent(ev, params) {
+        this.props.onEvent && this.props.onEvent(ev, params);
     }
 
     render() {
@@ -93,13 +133,15 @@ export class TreeView extends Component {
                 { this.state.nodes.map(x => <TreeNode 
                     onEvent={this.onEvent.bind(this)} 
                     displayname={x.displayname}
+                    isRoot={true}
+                    open={x.open || false}
                     children={x.children || []}
                     _id={x._id}
                 />) }
 
-                <div class="tree-node-add-child" onClick={this.props.onEvent.bind(this, 'addCategory')}>
+                <div class="tree-node-add-child" onClick={this.props.onEvent.bind(this, 'addroot', {})}>
                     <i class="far fa-plus"></i> 
-                    <span>Add category</span>
+                    <span>Add {this.props.topVerbatim || ""}</span>
                 </div>
             </div>
         )
