4a568b9207c38368a7ad8ffaf9133fd99b0b08f0
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2018-11-13 21:05:28 -0500

    Restructured paymentdashboard files for to make it clearer

---
+++ apps/lilium/pages/paymentsdashboard/creditcard.js
@@ -0,0 +1,18 @@
+export class CreditCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    componentDidMount() {
+        
+    }
+    
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
