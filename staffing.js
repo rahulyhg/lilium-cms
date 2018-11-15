d73b58a0069cf53d335ec0d91511d9cadb539bb7
anon <erik.desjardins@gmail.com>
2018-11-14 20:10:57 -0500

    Staffing controller, termination, restore, revoking

--- staffing.js
+++ staffing.js
@@ -8,12 +8,48 @@ const BUNCH_PROJECTION = {
     address : 1,
 
     "entity._id" : 1, "entity.displayname" : 1, "entity.avatarURL" : 1,
-    "entity.phone" : 1, "entity.username" : 1, "entity.email" : 1
+    "entity.phone" : 1, "entity.username" : 1, "entity.email" : 1, "entity.revoked" : 1
 };
 
 class Staffing {
+    adminDELETE(cli) {
+        if (!cli.hasRight('manage-staff')) {
+            return cli.throwHTTP(401, undefined, true);
+        }
+
+        if (cli.routeinfo.path[2] == "terminate") {
+            db.update(configLib.default(), 'staffing', { _id : db.mongoID(cli.routeinfo.path[3]) }, { status : "terminated" }, () => {
+                db.update(configLib.default(), 'entities', { _id : db.mongoID(cli.routeinfo.path[4]) }, { revoked : true }, () => {
+                    cli.sendJSON({ revoked : true, terminated : true });
+                });
+            });
+        } else {
+            cli.throwHTTP(404, undefined, true);
+        }
+    }
+
     adminPOST(cli) {
 
+    }
+
+    adminPUT(cli) {
+        if (!cli.hasRight('manage-staff')) {
+            return cli.throwHTTP(401, undefined, true);
+        }
+
+        if (cli.routeinfo.path[2] == "restore") {
+            db.update(configLib.default(), 'staffing', { _id : db.mongoID(cli.routeinfo.path[3]) }, { status : "active" }, () => {
+                cli.sendJSON({ terminated : false });
+            });
+        } else if (cli.routeinfo.path[2] == "edit") {
+            cli.readPostData(dat => {
+                db.update(configLib.default(), 'staffing', { _id : db.mongoID(cli.routeinfo.path[3]) }, { [dat.field] : dat.value }, () => {
+                    cli.sendJSON(dat);
+                });
+            });
+        } else {
+            cli.throwHTTP(404, undefined, true);
+        }
     }
 
     livevar(cli, levels, params, sendback) {
@@ -22,8 +58,13 @@ class Staffing {
                 sendback({err, staffinfo});
             });
         } else if (levels[0] == "single" && cli.hasRight('manage-staff')) {
-            db.findUnique(configLib.default(), 'staffing', { _id: db.mongoID(levels[1]) }, (err, staffinfo = {}) => {
-                sendback({err, staffinfo});
+            db.join(configLib.default(), 'staffing', [
+                { $match : { _id : db.mongoID(levels[1]) } },
+                { $lookup : { from : 'entities', as : 'entity', localField : 'entityid', foreignField : '_id' } },
+                { $project : BUNCH_PROJECTION },
+                { $unwind : "$entity" }
+            ], arr => {
+                sendback({ staff : arr[0] });
             });
         } else if (levels[0] == "bunch" && cli.hasRight('manage-staff')) {
             const $match = {
