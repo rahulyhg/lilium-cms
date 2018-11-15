580adfc0c8300492dc4f9ecd61ad447ebfd8666d
anon <erik.desjardins@gmail.com>
2018-11-14 23:57:29 -0500

    FormFields will handle value change in vanilla

--- apps/lilium/widgets/form.js
+++ apps/lilium/widgets/form.js
@@ -1,9 +1,9 @@
 import { h, Component } from "preact";
-import API from '../data/api';
 import flatpickr from 'flatpickr';
+import slugify from "slugify";
+import API from '../data/api';
 import { Spinner } from '../layout/loading'
 import { Picker } from '../layout/imagepicker';
-import slugify from "slugify";
 import { ImagePicker } from '../layout/imagepicker';
 
 class FormField extends Component {
@@ -110,13 +110,23 @@ export class ButtonWorker extends Component {
 }
 
 export class SelectField extends FormField {
+    componentDidMount() {
+        this.ref.value = this.value;
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.value) {
+            this.ref.value = props.value;
+        }
+    }
+
     render() {
         return (
             <div class="field-wrap">
                 { this.props.placeholder ? <b class="placeholder">{this.props.placeholder}</b> : null }
-                <select class="classic-field" value={this.props.initialValue} onChange={ev => this.changed(ev.target.value)}>
+                <select ref={x => (this.ref = x)} class="classic-field" onChange={ev => this.changed(ev.target.value)}>
                     { this.props.options.map(opt => (
-                        <option value={opt.value} key={opt.value} selected={opt.value == this.props.initialValue}>{opt.displayname|| opt.value}</option>
+                        <option value={opt.value} key={opt.value}>{opt.text || opt.displayname || opt.value}</option>
                     )) }
                 </select>
             </div> 
@@ -138,6 +148,10 @@ export class TextField extends FormField {
         } else {
             this.inputbox.value = '';
         }
+    }
+
+    componentDidMount() {
+        this.inputbox.value = this.value;
     }
 
     shouldComponentUpdate(nextProps) {
@@ -163,8 +177,8 @@ export class TextField extends FormField {
                 {
                     this.props.multiline ? 
                         ( <textarea placeholder={this.props.placeholderType == "inside" ? this.props.placeholder : ""} class="classic-field" style={this.props.style || {}} 
-                                    onChange={ev => this.changed(ev.target.value)} onKeyDown={this.handleKeyPress.bind(this)} ref={el => {this.inputbox = el}}>{this.props.initialValue || this.props.value || this.value || ""}</textarea>) :
-                        ( <input class={"classic-field text-field " + (this.state.saving ? "saving" : "")} placeholder={this.props.placeholderType == "inside" ? this.props.placeholder : ""} style={Object.assign({}, this.props.style || {})} type={this.props.type || 'text'} value={this.value}
+                                    onChange={ev => this.changed(ev.target.value)} onKeyDown={this.handleKeyPress.bind(this)} ref={el => {this.inputbox = el}}></textarea>) :
+                        ( <input class={"classic-field text-field " + (this.state.saving ? "saving" : "")} placeholder={this.props.placeholderType == "inside" ? this.props.placeholder : ""} style={Object.assign({}, this.props.style || {})} type={this.props.type || 'text'}
                                     onChange={ev => this.changed(ev.target.value)} onKeyDown={this.handleKeyPress.bind(this)} onBlur={this.props.onBlur && this.props.onBlur.bind(this)}
                                     onFocus={this.props.onFocus && this.props.onFocus.bind(this)} ref={el => {this.inputbox = el}} />)
                 }
@@ -659,6 +673,12 @@ export class DatePicker extends FormField {
                 this.changed( value ); 
             }
         });
+    }
+
+    componentWillReceiveProps(props) {
+        if (props.value) {
+            this.picker.setDate(new Date(props.value));
+        }
     }
 
     render() {
