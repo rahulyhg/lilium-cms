4d02eac08f44065b062fd4be49f5be340d656eca
anon <erik.desjardins@gmail.com>
2018-11-17 14:24:11 -0500

    Documented main, link, URLRenderer

--- apps/lilium/main.js
+++ apps/lilium/main.js
@@ -1,3 +1,71 @@
+/* 
+ *  ░░      ░░ ░░      ░░ ░░    ░░ ░░░    ░░░     ░░░░░░ ░░░    ░░░ ░░░░░░░    ░░░     ░░░     ░░   
+ *  ░░      ░░ ░░      ░░ ░░    ░░ ░░░░  ░░░░    ░░      ░░░░  ░░░░ ░░          ░░░   ░░░   ░░ ░░   
+ *  ░░      ░░ ░░      ░░ ░░    ░░ ░░ ░░░░ ░░    ░░      ░░ ░░░░ ░░ ░░░░░░░      ░░░ ░░░   ░░  ░░   
+ *  ░░      ░░ ░░      ░░ ░░    ░░ ░░  ░░  ░░    ░░      ░░  ░░  ░░      ░░       ░░░░░   ░░░░░░░░░ 
+ *  ░░░░░░░ ░░ ░░░░░░░ ░░  ░░░░░░  ░░      ░░     ░░░░░░ ░░      ░░ ░░░░░░░        ░░░         ░░   
+ *
+ *  Preact app without any boilerplate or heavy dependency for Lilium Version 4+
+ *
+ *  The project uses a custom "router" called the URLRenderer. It is possible for components
+ *  to register themselves in the URLRenderer, and define new routes.  
+ *
+ *  This is the main entry point of the app. The markup file using the app is located in the
+ *  same directory as the entry point, and is names "index.html.js". Custom logic can be
+ *  inserted directly in the index file, making is possible for plugin to require their 
+ *  own extension of Lilium. 
+ *
+ *  ------------------------------------------------------------------------------------------------
+ *
+ *  DIRECTORY STRUCTURE
+ *
+ *  data/
+ *      api.js              Contains wrappers to the Lilium API.
+ *      cache.js            Small utilities to make cache management easier.
+ *      const.js            All constants and hardcoded values.
+ *      logger.js           Logging function made global during bootstrapping.
+ *      session.js          Class representing a user session with entity info.
+ *      vocab.js            Internationalization utility made global during bootstrapping.
+ *
+ *  dev/
+ *      env.js              Development features such as JS bundle from frontend and more
+ *
+ *  layout/
+ *      cakepopsmanager.js  Overlay used to display Cakepops which are fullscreen messages
+ *      embedpicker.js      Extends picker.js
+ *      header.js           CMS header bar with notification dropdown and logo
+ *      imagepicker.js      Extends picker.js
+ *      loading.js          Fullscreen loading and spinner widget
+ *      lys.js              Poweruser utility to make navigation and quick actions available
+ *      menu.js             Sidebar menu
+ *      notifications.js    Realtime active and passive notifications at the bottom
+ *      picker.js           Fullscreen picker with upload capabilities
+ *      placepicker.js      Extends picker.js
+ *      titles.js           [Unused] Should represent a page title
+ *      vectors.js          SVG wrapper with relative path to vector assets
+ *
+ *  realtime/
+ *      connection.js       Functions used for binding on realtime events
+ *      lmlsocket.js        Abstraction of websocket with utility methods
+ *
+ *  routing/
+ *      link.js             Component triggering a router navigation on click
+ *      urlrenderer.js      Lilium's full featured router
+ *
+ *  widgets/
+ *      animenumber.js      A component that displays an animated number
+ *      articlepicker.js    Form field used to select articles
+ *      biglist.js          A lazyloading list of items with network calls and filters
+ *      chart.js            Simple wrapper around the library ChartJs
+ *      form.js             Multiple form fields including multiselect box and tags
+ *      modal.js            Fullscreen overlay with a modal
+ *      statusindicator.js  [Unused] SHould represent a status bubble
+ *      tabview.js          Multiple components with a tab selector
+ *      texteditor.js       Wrapper around the library TinyMCE
+ *      timeago.js          Utility functions to convert timespans into the "ago" format
+ *      treeview.js         Representation of data in the form of a tree (directory, files)
+ *
+ */
 import { Component, h, render } from 'preact';
 import { makeGlobalLogger } from './data/logger';
 import { Header } from './layout/header'
@@ -17,22 +85,27 @@ import { CakepopWrapper } from './layout/cakepopsmanager';
 
 import API from './data/api';
 
+// Global access to session, connection state, URL
 window.liliumcms = window.liliumcms || {};
 liliumcms.connected = true;
 
+// Create `log` function
 makeGlobalLogger();
+
+// Create `_v` language function
 makeGLobalLang();
 
+// If in dev mode, initialize dev functions
 if (liliumcms.env == "dev") {
     initializeDevEnv();
 }
 
+// Main app component
 class Lilium extends Component {
     constructor(props) {
         super(props);        
         this.state = {
             loading : true
-            /* session, menus, headerTitle */
         };
 
         // Initialize cache
@@ -48,6 +121,7 @@ class Lilium extends Component {
         log('Lilium', 'Main component finished mounting', 'lilium');
         this.fetchUserData();
 
+        // Fired when socket disconnects
         bindRealtimeEvent('disconnect', ev => {
             window.liliumcms.connected = false;
             log('Socket', "Lost internet connection, notifying user", 'socket');
@@ -59,10 +133,13 @@ class Lilium extends Component {
             });
         });
 
+        // Fired when socket connects after disconnecting
         bindRealtimeEvent('reconnect', ev => {
             log('Socket', "Internet connection restored, notifying user", 'socket');
 
             window.liliumcms.connected = true;
+
+            // Process all pending requests from when socket was offline
             API.processPendingRequests();
 
             castNotification({
@@ -72,6 +149,7 @@ class Lilium extends Component {
             });
         });
 
+        // Any component can cast the exit screen using an event
         document.addEventListener('castexitscreen', () => {
             this.setState({ loading : true });
         });
@@ -79,25 +157,37 @@ class Lilium extends Component {
 
     fetchUserData() { 
         log('Lilium', 'Requesting current session information', 'lilium');
+
+        // Fetch current user, admin menus, simple version of all entities
         API.getMany([
             { endpoint : '/me', params : {} },
             { endpoint : "/adminmenus", params : {} },
             { endpoint : "/entities/simple", params : {} }
         ], (err, resp) => {
+            // If no session found or at least one error detected, abort and output
             if (!resp["/me"] || !resp["/me"][0] || Object.keys(err).filter(x => err[x]).length != 0) {
                 this.setState({ error : Object.keys(err).map(x => err[x]).join(', '), loading : false });
             } else {
                 log('Lilium', 'Hello, ' + resp["/me"][0].displayname + '!', 'success');
                 const currentLanguage = resp['/me'][0].language || 'en-ca';
+
+                // Set user language before rendering the UI
                 setLanguage(currentLanguage, () => {
+                    
+                    // Store all entities in session storage as well as mapped version of array
                     setSession("entities", resp["/entities/simple"]);
                     setSession("mappedEntities", mapUsers(resp["/entities/simple"]));
+
+                    // Create new session object with current user's information
                     liliumcms.session = new LiliumSession(resp["/me"][0]);
+
+                    // Set allowed endpoints according to admin menus, plus hardcoded ones
                     liliumcms.session.setAllowedEndpoints([
                         "me", "preferences", "logout", "notifications", 
                         ...resp["/adminmenus"].map(x => x.absURL.split('/')[1])
                     ]);
 
+                    // Let the show begins
                     this.setState({ session : liliumcms.session, menus : resp["/adminmenus"], loading : false, currentLanguage });            
                 });
             }   
@@ -106,16 +196,19 @@ class Lilium extends Component {
 
     render() {
         log('Lilium', 'Rendering Lilium application into DOM', 'lilium');
+        // Original state, full screen loading view
         if (this.state.loading) {
             return (<LoadingView />);
         }
 
+        // Error view incase bootstrapping fails
         if (this.state.error) {
             return (
                 <div>Error loading Lilium : {this.state.error}</div>
             )
         }
 
+        // Marvelous chaos 
         return (
             <div id="lilium">
                 <Header session={this.state.session} />
