681b1b6e358c32fecb7edf25539c258da73f86ac
Gabriel Cardinal <gabriel.cardinal@narcitymedia.com>
2018-11-20 19:48:10 -0500

    Various front end fixes

--- apps/lilium/pages/paymentsdashboard/index.js
+++ apps/lilium/pages/paymentsdashboard/index.js
@@ -2,11 +2,15 @@ import { h, Component } from 'preact'
 import { TabView, Tab } from '../../widgets/tabview';
 import { PaymentsManagement } from './paymentsmanagement';
 import { CreditCardsManagement } from './creditcardsmanagement';
+import { InvoicesHistory } from './invoiceshistory';
 
 const PaymentDashboard = props => (
     <TabView>
         <Tab title='Pending Payments'>
             <PaymentsManagement />
+        </Tab>
+        <Tab title='Invoice History'>
+            <InvoicesHistory />
         </Tab>
         <Tab title='Credit Cards Management'>
             <CreditCardsManagement />
