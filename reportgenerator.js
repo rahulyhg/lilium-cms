710996b53511280d270fc6e7b0b53fb309abaf29
anon <erik.desjardins@gmail.com>
2018-11-21 14:55:07 -0500

    Scaffolding for report class

--- reportgenerator.js
+++ reportgenerator.js
@@ -1,3 +1,25 @@
+class ContractorReport {
+    static get defaultFilters() {
+        const now = new Date();
+        const defaultPast = new Date(now.getFullYear(), 0, 1, 0, 0, 0);
+
+        return {
+            daterange : [defaultPart, now],
+            currency : 'cad',
+            totalrange : [0, Infinity]
+        };
+    }
+
+    constructor(filters = {}, options = {}) {
+        this.filters = Object.assign(ContractorReport.defaultFilters, filters);
+        this.
+    }
+
+    generate() {
+
+    }
+}
+
 class ReportGenerator {
     livevar(cli, levels, params, sendback) {
         if (levels[0] == 'presets') {
@@ -21,3 +43,4 @@ class ReportGenerator {
 }
 
 module.exports = new ReportGenerator();
+
