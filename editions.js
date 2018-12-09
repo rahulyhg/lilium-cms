09316eba20baa47e1582c267428c4a8e933d11e0
anon <erik.desjardins@gmail.com>
2018-12-09 16:36:03 -0500

    Editions livevar, started working on adminPOST for merge

--- editions.js
+++ editions.js
@@ -13,7 +13,17 @@ class Edition {
 
 class EditionController {
     adminPOST(cli) {
-        
+        const act = cli.routeinfo.path[2];
+
+        if (act == "merge") {
+            // /merge/parent/futuremergee
+            const parent = db.mongoID(cli.routeinfo.path[3]);
+            const mergee = db.mongoID(cli.routeinfo.path[4]);
+
+            
+        } else {
+            cli.throwHTTP(404, undefined, 'Unknown level ' + act);
+        }
     }
 
     adminPUT(cli) {
@@ -21,11 +31,33 @@ class EditionController {
     }
 
     adminDELETE(cli) {
+        
+    }
 
+    getFull(_id, sendback) {
+        db.findUnique(cli._c, 'editions', { _id }, (err, ed) => {
+            sendback(ed);
+        });
     }
 
     livevar(cli, levels, params, done) {
-        
+        const act = levels[0];
+
+        if (act == "level") {
+            db.findToArray(cli._c, 'editions', { level : parseInt(levels[1]) }, (err, eds) => {
+                done(eds);
+            });
+        } else if (act == "full") {
+            this.getFull(db.mongoID(levels[1]), ed => done(ed));
+        } else if (act == "all") {
+            db.findToArray(cli._c, 'editions', {}, (err, eds) => {
+                done(eds);
+            }, {
+                displayname : 1, slug : 1, level : 1
+            });
+        } else {
+            done({ error : "Unknown level " + act });
+        }
     }
 }
 
