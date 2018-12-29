8cccd5ae36ea98f3f80f41c7b156e58519a8f713
anon <erik.desjardins@gmail.com>
2018-12-29 17:26:21 -0500

    Selection dropdown with LESS rules

--- apps/lilium/pages/editions/index.js
+++ apps/lilium/pages/editions/index.js
@@ -4,52 +4,44 @@ import { Spinner } from '../../layout/loading';
 
 import API from '../../data/api';
 
-class EditionSectionDropDown extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open : false,
-            selectedLevel : props.selectedLevel || 0
-        };
-    }
-
-    componentWillReceiveProps(props) {
-        this.setState({ selectedLevel : props.selectedLevel }); 
-    }
-
-    levelClicked(level) {
-        this.props.levelClicked(level);
-    }
-
-    render() {
-        return (
-            <div>
-                <div class="section-header">
-                    <b>{this.props.sections[this.state.selectedLevel].displayname}</b>
-                    <i class="far fa-chevron-down"></i>
-                </div>
-            </div>
-        )
-    }
-}
+const EditionSectionDropDown = props => props.open ? (
+    <div>
+        <div class="section-picker-open">
+            {
+                props.sections.map(sec => (
+                    <div class="single-section-to-pick" onClick={() => props.switchLevel(sec.level)}>
+                        <b>{sec.displayname}</b>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+) : (
+    <div>
+        <div class="section-header" onClick={() => props.toggleOpenState(true)}>
+            <b>{props.sections[props.level].displayname}</b>
+            <i class="far fa-chevron-down"></i>
+        </div>
+    </div>
+);
 
 export default class EditionPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading : true
+            loading : true,
+            draweropen : false
         };
     }
 
     componentDidMount() {
-        API.get('/editions/all', {}, (err, data) => {
-            this.setState({ levels : data.levels, sections : data.sections, selectedLevel : 0, loading : false });
-        });
+        API.get('/editions/all', {}, (err, data) => this.setState({ 
+            levels : data.levels, 
+            sections : data.sections, 
+            selectedLevel : 0, 
+            loading : false 
+        }));
     }
-
-    changeLevel(selectedLevel) {
-        this.setState({ selectedLevel })
-    }   
 
     render() {
         if (this.state.loading) {
@@ -59,7 +51,13 @@ export default class EditionPage extends Component {
         return (
             <div class="editions-management">
                 <div class="editions-sidebar">
-                    <EditionSectionDropDown levelClicked={this.changeLevel.bind(this)} sections={this.state.sections} level={this.state.selectedLevel} />
+                    <EditionSectionDropDown 
+                        switchLevel={selectedLevel => this.setState({ selectedLevel, draweropen : false })}
+                        toggleOpenState={draweropen => this.setState({ draweropen })} 
+
+                        sections={this.state.sections} 
+                        open={this.state.draweropen}
+                        level={this.state.selectedLevel} />
                 </div>
                 <div class="editions-editview">
                     
