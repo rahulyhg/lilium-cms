9bcd330e5aef6576240073e1f5fda5eb04e8553f
anon <erik.desjardins@gmail.com>
2018-12-29 21:42:19 -0500

    Edition page multi select sidebar + LESS

--- apps/lilium/pages/editions/index.js
+++ apps/lilium/pages/editions/index.js
@@ -5,23 +5,35 @@ import { Spinner } from '../../layout/loading';
 import API from '../../data/api';
 
 const EditionSectionDropDown = props => props.open ? (
-    <div>
-        <div class="section-picker-open">
-            {
-                props.sections.map(sec => (
-                    <div class="single-section-to-pick" onClick={() => props.switchLevel(sec.level)}>
-                        <b>{sec.displayname}</b>
-                    </div>
-                ))
-            }
-        </div>
+    <div class="section-picker-open">
+        {
+            props.sections.map(sec => (
+                <div key={sec._id} class="single-section-to-pick" onClick={() => props.switchLevel(sec.level)}>
+                    <b>{sec.displayname}</b>
+                </div>
+            ))
+        }
     </div>
 ) : (
-    <div>
-        <div class="section-header" onClick={() => props.toggleOpenState(true)}>
-            <b>{props.sections[props.level].displayname}</b>
+    <div class="section-header">
+        <div class="section-header-click-area" onClick={() => props.toggleOpenState(true)}>
+            <b>{props.level.displayname}</b>
             <i class="far fa-chevron-down"></i>
         </div>
+    </div>
+);
+
+const EditionList = props => (
+    <div class="edition-list">
+        { props.editions.map(ed => (<div class={"editions-sidebar-single " + (ed.selected ? "selected" : "")} onClick={() => props.selectEdition(ed)}>
+            <div class="ed-single-check">
+                <i class="far fa-check"></i>
+            </div>
+            <div class="ed-single-text">
+                <b>{ed.displayname}</b>
+                <small>/{"../".repeat(props.level)}{ed.slug}</small>
+            </div>
+        </div>)) }
     </div>
 );
 
@@ -30,7 +42,8 @@ export default class EditionPage extends Component {
         super(props);
         this.state = {
             loading : true,
-            draweropen : false
+            draweropen : false,
+            selectedEditions : []
         };
     }
 
@@ -43,6 +56,20 @@ export default class EditionPage extends Component {
         }));
     }
 
+    toggleSelectedEdition(ed) {
+        const arr = [...this.state.selectedEditions];
+        const index = arr.findIndex(x => x._id == ed._id);
+        index == -1 ? arr.push(ed) : arr.splice(index, 1); 
+        ed.selected = !ed.selected;
+
+        this.setState({ selectedEditions : arr })
+    }
+
+    switchLevel(selectedLevel) {
+        this.state.selectedEditions.forEach(x => x.selected = false);
+        this.setState({ selectedLevel, draweropen : false, selectedEditions : [] })
+    }
+
     render() {
         if (this.state.loading) {
             return (<Spinner centered />)
@@ -52,15 +79,21 @@ export default class EditionPage extends Component {
             <div class="editions-management">
                 <div class="editions-sidebar">
                     <EditionSectionDropDown 
-                        switchLevel={selectedLevel => this.setState({ selectedLevel, draweropen : false })}
+                        switchLevel={selectedLevel => this.switchLevel(selectedLevel)}
                         toggleOpenState={draweropen => this.setState({ draweropen })} 
 
                         sections={this.state.sections} 
                         open={this.state.draweropen}
-                        level={this.state.selectedLevel} />
+                        level={this.state.sections[this.state.selectedLevel]} />
+
+                    <EditionList 
+                        selectEdition={selectedEdition => this.toggleSelectedEdition(selectedEdition)}
+                        selectedEditions={this.state.selectedEditions}
+                        level={this.state.selectedLevel} 
+                        editions={this.state.levels[this.state.selectedLevel].editions} />
                 </div>
                 <div class="editions-editview">
-                    
+                    <p>{this.state.selectedEditions.map(x => x.displayname).join(', ')}</p>
                 </div>
             </div>
         );
