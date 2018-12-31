c097e5434e195c18da524aab17b5e678e83f008a
anon <erik.desjardins@gmail.com>
2018-12-31 16:32:07 -0500

    Edition page with settings loaded from theme, lang

--- apps/lilium/pages/editions/index.js
+++ apps/lilium/pages/editions/index.js
@@ -1,8 +1,11 @@
 import { h, Component } from 'preact';
-import { TextField } from '../../widgets/form';
+import { TextField, ButtonWorker, MediaPickerField } from '../../widgets/form';
+import { TextEditor } from '../../widgets/texteditor';
 import { Spinner } from '../../layout/loading';
 
 import API from '../../data/api';
+
+const MULTIPLE_VALUE_STRING = "[Multiple values]";
 
 const EditionSectionDropDown = props => props.open ? (
     <div class="section-picker-open">
@@ -38,7 +41,80 @@ const EditionList = props => (
 );
 
 class EditionEdit extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            language : "en"
+        };
+    }
 
+    fieldClassFromType(type) {
+        return TextField;
+    }
+
+    makeThemeField(field, editions) {
+        const FieldClass = this.fieldClassFromType(field.type);
+
+        return (<FieldClass name={field.name} placeholder={field.displayname} {...field.props} />);
+    }
+
+    changeLanguage(language) {
+        this.setState({ language });
+    }
+
+    render() {
+        return (
+            <div class="manage-editions">
+                <div class="edition-language-picker">
+                    <div onClick={this.changeLanguage.bind(this, 'en')} class={"edition-lang-pick " + (this.state.language == "en" ? "selected" : "")}>English</div>
+                    <div onClick={this.changeLanguage.bind(this, 'fr')} class={"edition-lang-pick " + (this.state.language == "fr" ? "selected" : "")}>Français</div>
+                </div>
+
+                { this.props.editions.length == 1 ? (
+                    <div class="card">
+                        <h3>Native settings</h3>
+                        <TextField placeholder="Display name" name="displayname" initialValue={this.props.editions[0].displayname} />
+                        <TextField placeholder="URL slug" name="slug" initialValue={this.props.editions[0].slug} />
+                        <TextEditor placeholder="Description" name="description" content={this.props.editions[0].description || ""} />
+                        <MediaPickerField placeholder="Icon" name="icon" initialValue={this.props.icon} size="small" />
+                        <MediaPickerField placeholder="Featured image" name="featuredimage" initialValue={this.props.featuredimage} />
+                    </div>
+                ) : (
+                    <div class="card">
+                        <h3>Native settings</h3>
+                        <p>Native fields of multiple editions cannot be edited at the same time.</p>
+                    </div>
+                ) }
+
+                { this.props.theme.editionForm ? (
+                    <div class="card">
+                        <h3>{this.props.theme.dName || "Theme"} settings</h3>
+                        { this.props.theme.editionForm.map(field => (
+                            this.makeThemeField(field, this.props.editions)
+                        )) }
+                    </div>
+                ) : null }
+
+                <div class="card">
+                    { this.props.editions.length == 1 ? (
+                        <div>
+                            <h3>Manage edition</h3>
+                            <ButtonWorker theme="red" type="outline" text="Delete edition" />
+                            <ButtonWorker theme="white" type="outline" text="Duplicate edition" />
+                            <ButtonWorker theme="white" type="outline" text="Change level" />
+                        </div>
+                    ) : (
+                        <div>
+                            <h3>Manage multiple editions</h3>
+                            <ButtonWorker theme="red" type="outline" text="Delete editions" />
+                            <ButtonWorker theme="white" type="outline" text="Merge editions" />
+                            <ButtonWorker theme="white" type="outline" text="Change level" />
+                        </div>
+                    ) }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default class EditionPage extends Component {
@@ -52,12 +128,16 @@ export default class EditionPage extends Component {
     }
 
     componentDidMount() {
-        API.get('/editions/all', {}, (err, data) => this.setState({ 
-            levels : data.levels, 
-            sections : data.sections, 
-            selectedLevel : 0, 
-            loading : false 
-        }));
+        API.get('/themes/current', {}, (terr, theme) => {
+            API.get('/editions/all', {}, (err, data) => this.setState({ 
+                theme,
+
+                levels : data.levels, 
+                sections : data.sections, 
+                selectedLevel : 0, 
+                loading : false 
+            }));
+        });
     }
 
     toggleSelectedEdition(ed) {
@@ -105,7 +185,9 @@ export default class EditionPage extends Component {
                                 <p>Use the sidebar to select editions.</p>
                             </div>
                         </div>
-                    ) : <EditionEdit editions={this.state.selectedEditions} /> }
+                    ) : (
+                        <EditionEdit theme={this.state.theme} editions={this.state.selectedEditions} /> 
+                    ) }
                 </div>
             </div>
         );
