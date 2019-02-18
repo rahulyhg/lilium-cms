d074411e712946fefb5cd6e14ebaa9957a2ed627
rykdesjardins <erik.desjardins@gmail.com>
2019-02-17 23:48:33 -0500

    Created script to find all dependents of a file

--- printdeptree.js
+++ printdeptree.js
@@ -1,10 +1,47 @@
-const dependencyTree = require('dependency-tree');
- 
-const deptree = dependencyTree({
-    filename: process.argv[2] || 'index.prod.js',
-    directory: __dirname,
-    filter: path => path.indexOf('node_modules') === -1, 
-    nonExistent: [] 
+const fs = require('fs');
+
+const rootFiles = [
+    ...fs.readdirSync('.').map(x => './' + x)
+].filter(x => x.endsWith('.js'));
+
+const deps = {};
+
+const getDeps = (file, pass = 0) => {
+    try {
+        const lines = fs.readFileSync(file).toString().split('\n');
+        deps[file] = deps[file] || [];
+
+        lines.forEach(line => {
+            const reg = /require\(["']([./a-zA-Z0-9]+)["']\)/;
+            const result = reg.exec(line);
+            if (result && result[1].startsWith('.')) {
+                let filename = result[1].replace(/\.\./g, '.');
+                if (!filename.endsWith('.js') && !filename.endsWith('.json')) {
+                    filename += '.js';
+                }
+
+                deps[file].push(filename);
+
+                if (pass < 10) {
+                    // getDeps(filename, pass + 1);
+                }
+            }
+        });
+
+        deps[file] = Array.from(new Set(deps[file]));
+    } catch (err) { }
+}
+
+rootFiles.forEach(x => {
+    getDeps(x);
 });
 
-console.log(JSON.stringify(deptree, undefined, 4));
+let singledep = process.argv[2];
+
+if (singledep && deps[singledep]) {
+    console.log("DEPENDENTS OF : " + singledep);
+    console.log();
+    Object.keys(deps).filter(x => deps[x].includes(singledep)).forEach(x => console.log(x));
+} else {
+    console.log(deps);
+}
