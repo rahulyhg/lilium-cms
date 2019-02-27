ccda2c459435b8b30a2bc020e6614e3233d0d8ff
rykdesjardins <erik.desjardins@gmail.com>
2019-02-27 14:42:49 -0500

    Setup will now instal default "Violet" theme

--- includes/setup.sh
+++ includes/setup.sh
@@ -18,12 +18,12 @@ mkdir sites
 mkdir flowers
 mkdir plugins
 
-echo "Input / Paste theme github repo URL : "
-read themeurl
+#echo "Input / Paste theme github repo URL : "
+#read themeurl
 
-echo "Cloning ${themeurl} in theme directory"
+echo "Cloning default theme into theme directory"
 cd flowers 
-git clone ${themeurl} 
+git clone https://github.com/narcitymedia/lilium-violet-theme 
 cd */
 npm install 
 
